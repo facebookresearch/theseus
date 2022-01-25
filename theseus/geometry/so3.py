e06850a58656cac04ad8cd5b9223f23562a6f1b7
Taosha Fan <fantaosha@gmail.com>
2022-01-25 15:46:44 -0800

    add SO3.hat() and SO3.vee()

--- theseus/geometry/so3.py
+++ theseus/geometry/so3.py
@@ -7,12 +7,15 @@ from typing import Optional, cast
 
 import torch
 
+import theseus.constants
+
 from .lie_group import LieGroup
 
 
 class SO3(LieGroup):
     def __init__(
         self,
+        quaternion: Optional[torch.Tensor] = None,
         data: Optional[torch.Tensor] = None,
         name: Optional[str] = None,
         dtype: Optional[torch.dtype] = None,
@@ -54,11 +57,32 @@ class SO3(LieGroup):
 
     @staticmethod
     def hat(tangent_vector: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
+        _check = tangent_vector.ndim == 3 and tangent_vector.shape[1:] == (3, 1)
+        _check |= tangent_vector.ndim == 2 and tangent_vector.shape[1] == 3
+        matrix = torch.zeros(tangent_vector.shape[0], 3, 3).to(
+            dtype=tangent_vector.dtype,
+            device=tangent_vector.device
+        )
+        matrix[:, 0, 1] = -tangent_vector[: , 2].view(-1)
+        matrix[:, 0, 2] = tangent_vector[: , 1].view(-1)
+        matrix[:, 1, 2] = -tangent_vector[: , 0].view(-1)
+        matrix[:, 1, 0] = tangent_vector[: , 2].view(-1)
+        matrix[:, 2, 0] = -tangent_vector[: , 1].view(-1)
+        matrix[:, 2, 1] = tangent_vector[: , 0].view(-1)
+        return matrix
 
     @staticmethod
     def vee(matrix: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
+        _check = matrix.ndim == 3 and matrix.shape[1:] == (3, 3)
+        _check &= (matrix.transpose(1, 2) + matrix).abs().max().item() < theseus.constants.EPS
+        if not _check:
+            raise ValueError("Invalid hat matrix for SO3.")
+        vec = torch.zeros(matrix.shape[0], 3)
+        vec[:, 0] = matrix[:, 2, 1]
+        vec[:, 1] = matrix[:, 0, 2]
+        vec[:, 2] = matrix[:, 1, 0]
+
+        return vec
 
     def _copy_impl(self, new_name: Optional[str] = None) -> "SO3":
         return SO3(data=self.data.clone(), name=new_name)

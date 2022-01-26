16bc85d48a76ff0e5627743f214d3287973190d8
Taosha Fan <fantaosha@gmail.com>
2022-01-25 20:40:13 -0800

    add SO3 and unit quaternion check for liegroup.SO3

--- theseus/geometry/so3.py
+++ theseus/geometry/so3.py
@@ -3,13 +3,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Optional, cast
+from typing import Optional, Union, cast
 
 import torch
 
 import theseus.constants
 
 from .lie_group import LieGroup
+from .point_types import Point3
 
 
 class SO3(LieGroup):
@@ -20,11 +21,20 @@ class SO3(LieGroup):
         name: Optional[str] = None,
         dtype: Optional[torch.dtype] = None,
     ):
-        raise NotImplementedError
+        if quaternion is not None and data is not None:
+            raise ValueError("Please provide only one of theta or data.")
+
+        if quaternion is not None:
+            dtype = quaternion.dtype
+        super().__init__(data=data, name=name, dtype=dtype)
+
+        if quaternion is not None:
+            if quaternion.ndim == 1:
+                quaternion = quaternion.unsqueeze(0)
 
     @staticmethod
     def _init_data() -> torch.Tensor:  # type: ignore
-        raise NotImplementedError
+        raise torch.eye(3, 3).veiw(1, 3, 3)
 
     def dof(self) -> int:
         return 3
@@ -34,10 +44,29 @@ class SO3(LieGroup):
 
     def __str__(self) -> str:
         with torch.no_grad():
-            raise NotImplementedError
+            return f"SO3(matrix={self.data}), name={self.name})"
 
     def _adjoint_impl(self) -> torch.Tensor:
-        raise NotImplementedError
+        raise self.data
+
+    @staticmethod
+    def _SO3_matrix_check(matrix: torch.Tensor):
+        if matrix.ndim != 3 or matrix.shape[1:] != (3, 3):
+            raise ValueError("3D rotations can only be 3x3 matrices.")
+        _check = (torch.linalg.matmul(matrix, matrix) - torch.eye(3, 3)
+                  ).abs().max().item() < theseus.constants.EPS
+        _check &= (torch.linalg.det(matrix) - 1).abs().max().item() < theseus.constants.EPS
+
+        if not _check:
+            raise ValueError("Not valid 3D rotations.")
+
+    @staticmethod
+    def _unit_quaternion_check(quaternion: torch.Tensor):
+        if quaternion.ndim != 2 or quaternion.shape[1] != 4:
+            raise ValueError("Quaternions can only be 4-D vectors.")
+
+        if (torch.linalg.norm(quaternion, dim=1) - 1).abs().max().item() >= theseus.constants.EPS:
+            raise ValueError("Not unit quaternions.")
 
     @staticmethod
     def exp_map(tangent_vector: torch.Tensor) -> LieGroup:
@@ -53,12 +82,17 @@ class SO3(LieGroup):
         raise NotImplementedError
 
     def to_matrix(self) -> torch.Tensor:
+        return self.data
+
+    def to_quaternion(self) -> torch.Tensor:
         raise NotImplementedError
 
     @staticmethod
     def hat(tangent_vector: torch.Tensor) -> torch.Tensor:
         _check = tangent_vector.ndim == 3 and tangent_vector.shape[1:] == (3, 1)
         _check |= tangent_vector.ndim == 2 and tangent_vector.shape[1] == 3
+        if not _check:
+            raise ValueError("Invalid vee matrix for SO3.")
         matrix = torch.zeros(tangent_vector.shape[0], 3, 3).to(
             dtype=tangent_vector.dtype,
             device=tangent_vector.device
@@ -81,8 +115,23 @@ class SO3(LieGroup):
         vec[:, 0] = matrix[:, 2, 1]
         vec[:, 1] = matrix[:, 0, 2]
         vec[:, 2] = matrix[:, 1, 0]
-
         return vec
+
+    def _rotate_shape_check(self, point: Union[Point3, torch.Tensor]):
+        err_msg = "SO3 can only rotate 3-D vectors."
+        if isinstance(point, torch.Tensor):
+            if not point.ndim == 2 or point.shape[1] != 3:
+                raise ValueError(err_msg)
+        elif point.dof() != 3:
+            raise ValueError(err_msg)
+        if (
+            point.shape[0] != self.shape[0]
+            and point.shape[0] != 1
+            and self.shape[0] != 1
+        ):
+            raise ValueError(
+                "Input point batch size is not broadcastable with group batch size."
+            )
 
     def _copy_impl(self, new_name: Optional[str] = None) -> "SO3":
         return SO3(data=self.data.clone(), name=new_name)

19d787dedfb90ca92530d5189203fa0a22c43b7c
Taosha Fan <fantaosha@gmail.com>
2022-02-02 12:50:46 -0800

    SE3.exp added but not tested

--- theseus/geometry/se3.py
+++ theseus/geometry/se3.py
@@ -97,7 +97,74 @@ class SE3(LieGroup):
 
     @staticmethod
     def exp_map(tangent_vector: torch.Tensor) -> LieGroup:
-        raise NotImplementedError
+        if tangent_vector.ndim != 2 or tangent_vector.shape[1] != 6:
+            raise ValueError("Tangent vectors of SE(3) can only be 6-D vectors.")
+        ret = SE3(dtype=tangent_vector.dtype)
+
+        NEAR_ZERO_EPS = 5e-3
+        tangent_vector_lin = tangent_vector[:, :3].view(-1, 3, 1)
+        tangent_vector_ang = tangent_vector[:, 3:].view(-1, 3, 1)
+
+        theta = torch.linalg.norm(tangent_vector_ang, dim=1).unsqueeze(1)
+        theta2 = theta**2
+        theta3 = theta**3
+
+        near_zero = theta < NEAR_ZERO_EPS
+        non_zero = torch.ones(
+            1, dtype=tangent_vector.dtype, device=tangent_vector.device
+        )
+        theta_nz = torch.where(near_zero, non_zero, theta)
+        theta2_nz = torch.where(near_zero, non_zero, theta2)
+        theta3_nz = torch.where(near_zero, non_zero, theta3)
+
+        # Compute the rotation
+        sine = theta.sin()
+        cosine = torch.where(near_zero, 8 / (4 + theta2) - 1, theta.cos())
+        sine_by_theta = torch.where(
+            near_zero, 0.5 * cosine + 0.5, theta.sin() / theta_nz
+        )
+        one_minus_cosie_by_theta2 = torch.where(
+            near_zero, 0.5 * sine_by_theta, (1 - cosine) / theta2_nz
+        )
+        ret.data = torch.zeros(tangent_vector.shape[0], 3, 4).to(
+            dtype=tangent_vector.dtype, device=tangent_vector.device
+        )
+        ret.data[:, :3, :3] = (
+            one_minus_cosie_by_theta2
+            * tangent_vector_ang
+            @ tangent_vector_ang.transpose(1, 2)
+        )
+
+        ret[:, 0, 0] += cosine.view(-1)
+        ret[:, 1, 1] += cosine.view(-1)
+        ret[:, 2, 2] += cosine.view(-1)
+        temp = sine_by_theta.view(-1, 1) * tangent_vector_ang.view(-1, 3)
+        ret[:, 0, 1] -= temp[:, 2]
+        ret[:, 1, 0] += temp[:, 2]
+        ret[:, 0, 2] += temp[:, 1]
+        ret[:, 2, 0] -= temp[:, 1]
+        ret[:, 1, 2] -= temp[:, 0]
+        ret[:, 2, 1] += temp[:, 0]
+
+        # Compute the translation
+        sine_by_theta = torch.where(near_zero, 1 - theta2 / 6, sine_by_theta)
+        one_minus_cosie_by_theta2 = torch.where(
+            near_zero, 0.5 - theta2 / 24, one_minus_cosie_by_theta2
+        )
+        theta_minus_sine_by_theta3 = torch.where(
+            near_zero, 1.0 / 6 - theta2 / 120, (theta - sine) / theta3_nz
+        )
+
+        ret[:, :, 3:] = sine_by_theta * tangent_vector_lin
+        ret[:, :, 3:] += one_minus_cosie_by_theta2 * torch.cross(
+            tangent_vector_ang, tangent_vector_lin
+        )
+        ret[:, :, 3:] += theta_minus_sine_by_theta3 * (
+            tangent_vector_ang
+            @ (tangent_vector_ang.transpose(1, 2) @ tangent_vector_lin)
+        )
+
+        return ret
 
     def _log_map_impl(self) -> torch.Tensor:
         raise NotImplementedError

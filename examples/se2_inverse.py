0c7c93ae8d1b24ab67f73a80c4eeaf1ff547b684
Taosha Fan <fantaosha@gmail.com>
2022-02-04 21:36:17 -0800

    add lr_scheduler for se2_inverse.py

--- examples/se2_inverse.py
+++ examples/se2_inverse.py
@@ -19,23 +19,28 @@ def run(x1, x2, num_iters=10, use_proj=True):
         x1.data = LieGroupTensor(x1)
     x1.data.requires_grad = True
     optim = torch.optim.Adam([x1.data], lr=1e-1)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        optim, milestones=[250, 600], gamma=0.01
+    )
     for i in range(num_iters):
         optim.zero_grad()
         cf = th.eb.VariableDifference(x1.inverse(), th.ScaleCostWeight(1.0), x2)
-        loss = cf.error().norm() ** 2
-        print(
-            "loss is {:.10f}, cos(theta)^2 + sin(theta)^2 is {:.10f}".format(
-                loss.item(), x1[0, 2:].norm().item() ** 2
+        loss = cf.error().norm()
+        if i % 100 == 0:
+            print(
+                "iter {:0>4d}: loss is {:.10f}, cos(theta)^2 + sin(theta)^2 is {:.10f}".format(
+                    i, loss.item(), x1[0, 2:].norm().item() ** 2
+                )
             )
-        )
         loss.backward()
         optim.step()
+        scheduler.step()
 
     cf = th.eb.VariableDifference(x1.inverse(), th.ScaleCostWeight(1.0), x2)
     loss = cf.error().norm()
     print(
-        "loss is {:.10f}, cos(theta)^2 + sin(theta)^2 is {:.10f}".format(
-            loss.item(), x1[0, 2:].norm().item() ** 2
+        "iter {}: loss is {:.10f}, cos(theta)^2 + sin(theta)^2 is {:.10f}".format(
+            num_iters, loss.item(), x1[0, 2:].norm().item() ** 2
         )
     )
 
@@ -43,10 +48,10 @@ def run(x1, x2, num_iters=10, use_proj=True):
 print("===============================================")
 print("Graident on the Lie Group Tangent Space")
 print("-----------------------------------------------")
-run(x1.copy(), x2.copy(), num_iters=10, use_proj=True)
+run(x1.copy(), x2.copy(), num_iters=1000, use_proj=True)
 
 print("\n")
 print("===============================================")
 print("Graident on the Euclidean Space")
 print("-----------------------------------------------")
-run(x1.copy(), x2.copy(), num_iters=10, use_proj=False)
+run(x1.copy(), x2.copy(), num_iters=1000, use_proj=False)

85b46096e6168768917fafe08e1e51323f92345b
Taosha Fan <fantaosha@gmail.com>
2022-02-04 18:46:15 -0800

    Add comparisions for se2_inverse

--- examples/se2_inverse.py
+++ examples/se2_inverse.py
@@ -12,29 +12,33 @@ rng.manual_seed(0)
 x1 = create_random_se2(1, rng)  # bug: creates a random tensor w/o using rng
 x2 = create_random_se2(1, rng)
 
-# LieGroupTensor defines how to update x1.data
-x1.data = LieGroupTensor(x1)
-optim = torch.optim.Adam([x1.data], lr=1e-1)
-for i in range(100):
-    optim.zero_grad()
-    cf = th.eb.VariableDifference(x1.inverse(), th.ScaleCostWeight(1.0), x2)
-    loss = cf.error().norm()
-    loss.backward()
-    grad_size = x1.data.grad.shape[1]
-    optim.step()
-    print("loss: ", loss.item())
-    print(f"cos(theta)^2 + sin(theta)^2 is {x1[0,2:].norm().item()}")
+
+def run(x1, x2, num_iters=10, use_proj=True):
+    # LieGroupTensor defines how to update x1.data
+    if use_proj:
+        x1.data = LieGroupTensor(x1)
+    x1.data.requires_grad = True
+    optim = torch.optim.Adam([x1.data], lr=1e-1)
+    for i in range(num_iters):
+        optim.zero_grad()
+        cf = th.eb.VariableDifference(x1.inverse(), th.ScaleCostWeight(1.0), x2)
+        loss = cf.error().norm()
+        loss.backward()
+        optim.step()
+        print(
+            "loss: {:.10f},  cos(theta)^2 + sin(theta)^2 is {:.10f}".format(
+                loss.item(), x1[0, 2:].norm().item()
+            )
+        )
 
 
-for param in optim.param_groups:
-    param["lr"] = 1e-3
+print("===============================================")
+print("Graident on the Lie Group Tangent Space")
+print("-----------------------------------------------")
+run(x1.copy(), x2.copy(), num_iters=10, use_proj=True)
 
-for i in range(100):
-    optim.zero_grad()
-    cf = th.eb.VariableDifference(x1.inverse(), th.ScaleCostWeight(1.0), x2)
-    loss = cf.error().norm()
-    loss.backward()
-    grad_size = x1.data.grad.shape[1]
-    optim.step()
-    print("loss: ", loss.item())
-    print(f"cos(theta)^2 + sin(theta)^2 is {x1[0,2:].norm().item()}")
+
+print("===============================================")
+print("Graident on the Euclidean Space")
+print("-----------------------------------------------")
+run(x1.copy(), x2.copy(), num_iters=10, use_proj=False)

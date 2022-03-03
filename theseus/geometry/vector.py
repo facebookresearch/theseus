ae8c2629aa517501793d1bede5707b401843835f
Taosha Fan <fantaosha@gmail.com>
2022-03-03 14:47:24 -0800

    add rand and randn to __init__.py

--- theseus/geometry/vector.py
+++ theseus/geometry/vector.py
@@ -219,5 +219,5 @@ class Vector(LieGroup):
         return cast(Vector, super().copy(new_name=new_name))
 
 
-rand_vector2 = Vector.rand
-randn_vector2 = Vector.randn
+rand_vector = Vector.rand
+randn_vector = Vector.randn

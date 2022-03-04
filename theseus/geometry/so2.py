Fix SO2 rotate and unrotate jacobian (#58)
* Jacobian bugs fixed for so2.rotate and so2.unrotate

* add shape check for test_so2.py

* add tests for SO3.rotate and SO3.unrotate

theseus/geometry/so2.py
@@ -183,7 +183,7 @@ class SO2(LieGroup):
         if jacobians is not None:
             self._check_jacobians_list(jacobians)
             J1 = torch.stack([-rotation.y(), rotation.x()], dim=1).view(-1, 2, 1)
-            J2 = self.to_matrix()
+            J2 = self.to_matrix().expand(rotation.shape[0], -1, -1)
             jacobians.extend([J1, J2])
         return rotation
 
@@ -198,7 +198,7 @@ class SO2(LieGroup):
         if jacobians is not None:
             self._check_jacobians_list(jacobians)
             J1 = torch.stack([rotation.y(), -rotation.x()], dim=1).view(-1, 2, 1)
-            J2 = self.to_matrix().transpose(2, 1)
+            J2 = self.to_matrix().transpose(2, 1).expand(rotation.shape[0], -1, -1)
             jacobians.extend([J1, J2])
         return rotation
 
c2de966e6dc2d0d9ea7d0cda953664a6f3389b20
Taosha Fan <fantaosha@gmail.com>
2022-03-07 10:21:54 -0800

    add rand seed to test_se3.py

--- theseus/geometry/tests/test_se3.py
+++ theseus/geometry/tests/test_se3.py
@@ -28,98 +28,108 @@ def check_SE3_log_map(tangent_vector, atol=EPS):
 
 
 def test_exp_map():
+    rng = torch.Generator()
+    rng.manual_seed(0)
+
     for batch_size in [1, 20, 100]:
-        tangent_vector_ang = torch.rand(batch_size, 3).double() - 0.5
+        tangent_vector_ang = torch.rand(batch_size, 3, generator=rng).double() - 0.5
         tangent_vector_ang /= tangent_vector_ang.norm(dim=1, keepdim=True)
-        tangent_vector_ang *= torch.rand(batch_size, 1).double() * 2 * np.pi - np.pi
-        tangent_vector_lin = torch.randn(batch_size, 3).double()
+        tangent_vector_ang *= (
+            torch.rand(batch_size, 1, generator=rng).double() * 2 * np.pi - np.pi
+        )
+        tangent_vector_lin = torch.randn(batch_size, 3, generator=rng).double()
         tangent_vector = torch.cat([tangent_vector_lin, tangent_vector_ang], dim=1)
 
         check_exp_map(tangent_vector, th.SE3)
 
     # SE3.exp_map uses approximations for small theta
     for batch_size in [1, 20, 100]:
-        tangent_vector_ang = torch.rand(batch_size, 3).double() - 0.5
+        tangent_vector_ang = torch.rand(batch_size, 3, generator=rng).double() - 0.5
         tangent_vector_ang /= tangent_vector_ang.norm(dim=1, keepdim=True)
         tangent_vector_ang *= 1e-5
-        tangent_vector_lin = torch.randn(batch_size, 3).double()
+        tangent_vector_lin = torch.randn(batch_size, 3, generator=rng).double()
         tangent_vector = torch.cat([tangent_vector_lin, tangent_vector_ang], dim=1)
 
         check_exp_map(tangent_vector, th.SE3)
 
     # SE3.exp_map uses the exact exponential map for small theta
     for batch_size in [1, 20, 100]:
-        tangent_vector_ang = torch.rand(batch_size, 3).double() - 0.5
+        tangent_vector_ang = torch.rand(batch_size, 3, generator=rng).double() - 0.5
         tangent_vector_ang /= tangent_vector_ang.norm(dim=1, keepdim=True)
         tangent_vector_ang *= 3e-3
-        tangent_vector_lin = torch.randn(batch_size, 3).double()
+        tangent_vector_lin = torch.randn(batch_size, 3, generator=rng).double()
         tangent_vector = torch.cat([tangent_vector_lin, tangent_vector_ang], dim=1)
 
         check_exp_map(tangent_vector, th.SE3)
 
     for batch_size in [1, 20, 100]:
-        tangent_vector_ang = torch.rand(batch_size, 3).double() - 0.5
+        tangent_vector_ang = torch.rand(batch_size, 3, generator=rng).double() - 0.5
         tangent_vector_ang /= tangent_vector_ang.norm(dim=1, keepdim=True)
         tangent_vector_ang *= 2 * np.pi - 1e-11
-        tangent_vector_lin = torch.randn(batch_size, 3).double()
+        tangent_vector_lin = torch.randn(batch_size, 3, generator=rng).double()
         tangent_vector = torch.cat([tangent_vector_lin, tangent_vector_ang], dim=1)
 
         check_exp_map(tangent_vector, th.SE3)
 
     for batch_size in [1, 20, 100]:
-        tangent_vector_ang = torch.rand(batch_size, 3).double() - 0.5
+        tangent_vector_ang = torch.rand(batch_size, 3, generator=rng).double() - 0.5
         tangent_vector_ang /= tangent_vector_ang.norm(dim=1, keepdim=True)
         tangent_vector_ang *= np.pi - 1e-11
-        tangent_vector_lin = torch.randn(batch_size, 3).double()
+        tangent_vector_lin = torch.randn(batch_size, 3, generator=rng).double()
         tangent_vector = torch.cat([tangent_vector_lin, tangent_vector_ang], dim=1)
 
         check_exp_map(tangent_vector, th.SE3)
 
 
 def test_log_map():
+    rng = torch.Generator()
+    rng.manual_seed(0)
+
     for batch_size in [1, 20, 100]:
-        tangent_vector_ang = torch.rand(batch_size, 3) - 0.5
+        tangent_vector_ang = torch.rand(batch_size, 3, generator=rng) - 0.5
         tangent_vector_ang /= tangent_vector_ang.norm(dim=1, keepdim=True)
-        tangent_vector_ang *= torch.rand(batch_size, 1).double() * 2 * np.pi - np.pi
-        tangent_vector_lin = torch.randn(batch_size, 3).double()
+        tangent_vector_ang *= (
+            torch.rand(batch_size, 1, generator=rng).double() * 2 * np.pi - np.pi
+        )
+        tangent_vector_lin = torch.randn(batch_size, 3, generator=rng).double()
         tangent_vector = torch.cat([tangent_vector_lin, tangent_vector_ang], dim=1)
 
         check_SE3_log_map(tangent_vector)
 
     # SE3.log_map uses approximations for small theta
     for batch_size in [1, 20, 100]:
-        tangent_vector_ang = torch.rand(batch_size, 3).double() - 0.5
+        tangent_vector_ang = torch.rand(batch_size, 3, generator=rng).double() - 0.5
         tangent_vector_ang /= tangent_vector_ang.norm(dim=1, keepdim=True)
         tangent_vector_ang *= 1e-5
-        tangent_vector_lin = torch.randn(batch_size, 3).double()
+        tangent_vector_lin = torch.randn(batch_size, 3, generator=rng).double()
         tangent_vector = torch.cat([tangent_vector_lin, tangent_vector_ang], dim=1)
 
         check_SE3_log_map(tangent_vector)
 
     # SE3.log_map uses the exact logarithm map for small theta
     for batch_size in [1, 20, 100]:
-        tangent_vector_ang = torch.rand(batch_size, 3).double() - 0.5
+        tangent_vector_ang = torch.rand(batch_size, 3, generator=rng).double() - 0.5
         tangent_vector_ang /= tangent_vector_ang.norm(dim=1, keepdim=True)
         tangent_vector_ang *= 3e-3
-        tangent_vector_lin = torch.randn(batch_size, 3).double()
+        tangent_vector_lin = torch.randn(batch_size, 3, generator=rng).double()
         tangent_vector = torch.cat([tangent_vector_lin, tangent_vector_ang], dim=1)
 
         check_SE3_log_map(tangent_vector)
 
     for batch_size in [1, 20, 100]:
-        tangent_vector_ang = torch.rand(batch_size, 3).double() - 0.5
+        tangent_vector_ang = torch.rand(batch_size, 3, generator=rng).double() - 0.5
         tangent_vector_ang /= tangent_vector_ang.norm(dim=1, keepdim=True)
         tangent_vector_ang *= 2 * np.pi - 1e-11
-        tangent_vector_lin = torch.randn(batch_size, 3).double()
+        tangent_vector_lin = torch.randn(batch_size, 3, generator=rng).double()
         tangent_vector = torch.cat([tangent_vector_lin, tangent_vector_ang], dim=1)
 
         check_SE3_log_map(tangent_vector)
 
     for batch_size in [1, 20, 100]:
-        tangent_vector_ang = torch.rand(batch_size, 3).double() - 0.5
+        tangent_vector_ang = torch.rand(batch_size, 3, generator=rng).double() - 0.5
         tangent_vector_ang /= tangent_vector_ang.norm(dim=1, keepdim=True)
         tangent_vector_ang *= np.pi - 1e-11
-        tangent_vector_lin = torch.randn(batch_size, 3).double()
+        tangent_vector_lin = torch.randn(batch_size, 3, generator=rng).double()
         tangent_vector = torch.cat([tangent_vector_lin, tangent_vector_ang], dim=1)
 
         check_SE3_log_map(tangent_vector)

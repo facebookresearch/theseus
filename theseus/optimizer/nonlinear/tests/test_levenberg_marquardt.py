8dba61c036809b6ac379e903143e5a1df40c7a02
Luis Pineda <luisen.p@gmail.com>
2022-03-08 13:18:56 -0500

    Enabled back ellipsoidal damping in LM with linear solvers support checks (#87)
    * Enabled back ellipsoidal damping in LM and added checks for supporting linear solvers.
    
    * Fixed bad dtype handling in Objective and SparseLinearization.
    
    * Renamed _LM_ALLOWED_SOLVERS to _LM_ALLOWED_ELLIPS_DAMP_SOLVERS.
    
    * Changed nox so that pytest only runs 'not cudaext' market tests.
    
    * Added batch_size kwarg to LUCudaSparseSolver.

--- theseus/optimizer/nonlinear/tests/test_levenberg_marquardt.py
+++ theseus/optimizer/nonlinear/tests/test_levenberg_marquardt.py
@@ -4,14 +4,24 @@
 # LICENSE file in the root directory of this source tree.
 
 import pytest  # noqa: F401
+import torch
 
 import theseus as th
 
 from .common import run_nonlinear_least_squares_check
 
 
+@pytest.fixture
+def mock_objective():
+    objective = th.Objective()
+    v1 = th.Vector(1, name="v1")
+    v2 = th.Vector(1, name="v2")
+    objective.add(th.eb.VariableDifference(v1, th.ScaleCostWeight(1.0), v2))
+    return objective
+
+
 def test_levenberg_marquartd():
-    for ellipsoidal_damping in [False]:
+    for ellipsoidal_damping in [True, False]:
         for damping in [0, 0.001, 0.01, 0.1]:
             run_nonlinear_least_squares_check(
                 th.LevenbergMarquardt,
@@ -22,3 +32,36 @@ def test_levenberg_marquartd():
                 },
                 singular_check=damping < 0.001,
             )
+
+
+def test_ellipsoidal_damping_compatibility(mock_objective):
+    mock_objective.update({"v1": torch.ones(1, 1), "v2": torch.zeros(1, 1)})
+    for lsc in [th.LUDenseSolver, th.CholeskyDenseSolver]:
+        optimizer = th.LevenbergMarquardt(mock_objective, lsc)
+        optimizer.optimize(ellipsoidal_damping=True)
+        optimizer.optimize(damping_eps=0.1)
+
+    for lsc in [th.CholmodSparseSolver]:
+        optimizer = th.LevenbergMarquardt(mock_objective, lsc)
+        with pytest.raises(RuntimeError):
+            optimizer.optimize(ellipsoidal_damping=True)
+        with pytest.raises(RuntimeError):
+            optimizer.optimize(damping_eps=0.1)
+
+
+@pytest.mark.cuda
+def test_ellipsoidal_damping_compatibility_cuda(mock_objective):
+    mock_objective.to(device="cuda", dtype=torch.double)
+    batch_size = 2
+    mock_objective.update(
+        {
+            "v1": torch.ones(batch_size, 1, device="cuda", dtype=torch.double),
+            "v2": torch.zeros(batch_size, 1, device="cuda", dtype=torch.double),
+        }
+    )
+    for lsc in [th.LUCudaSparseSolver]:
+        optimizer = th.LevenbergMarquardt(
+            mock_objective, lsc, linear_solver_kwargs={"batch_size": batch_size}
+        )
+        optimizer.optimize(ellipsoidal_damping=True)
+        optimizer.optimize(damping_eps=0.1)

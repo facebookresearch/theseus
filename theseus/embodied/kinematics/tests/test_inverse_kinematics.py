2ddce065c0fd5b9918e6ce82164a3a61da09659b
ExhAustin <aswang@andrew.cmu.edu>
2022-03-29 04:56:36 -0700

    Batched attempt

--- theseus/embodied/kinematics/tests/test_inverse_kinematics.py
+++ theseus/embodied/kinematics/tests/test_inverse_kinematics.py
@@ -5,7 +5,6 @@
 
 import os
 
-import numpy as np
 import pytest
 import torch
 
@@ -13,11 +12,8 @@ import theseus as th
 
 NUM_DOFS = 7
 EE_NAME = "panda_virtual_ee_link"
-HOME_POSE = [-0.1394, -0.0205, -0.0520, -2.0691, 0.0506, 2.0029, -0.9168]
-
-
-def sample_vector_dist(range):
-    return np.random.choice([-1, 1]) * range * torch.rand(range.shape)
+HOME_POSE = torch.Tensor([-0.1394, -0.0205, -0.0520, -2.0691, 0.0506, 2.0029, -0.9168])
+ERR_SCALING = torch.Tensor([1.0, 1.0, 1.0, 5.0, 5.0, 5.0])
 
 
 @pytest.fixture
@@ -26,50 +22,52 @@ def robot_model():
     return th.eb.UrdfRobotModel(urdf_path)
 
 
+@pytest.fixture(params=[1, 3])
+def batch_size(request):
+    return request.param
+
+
 @pytest.fixture
-def ee_pose_target():
-    np.random.seed(0)
-    torch.manual_seed(0)
+def ee_pose_target(batch_size):
+    torch.manual_seed(1)
 
     ee_pos_mid = torch.Tensor([0.6, 0.0, 0.5])
-    ee_pos_range = torch.Tensor([0.3, 0.5, 0.5])
+    ee_pos_range = torch.Tensor([0.2, 0.3, 0.3])
     ee_quat_mid = torch.Tensor([0.9383, 0.3442, -0.0072, -0.0318])
     ee_quat_range = torch.Tensor([0.5, 0.5, 0.5])
 
-    ee_pos_target = ee_pos_mid + sample_vector_dist(ee_pos_range)
+    ee_pos_dev = ee_pos_range * torch.randn(batch_size, 3)
+    ee_rot_dev = ee_quat_range * torch.randn(batch_size, 3)
+
+    ee_pos_target = ee_pos_mid + ee_pos_dev
     ee_quat_target = (
-        (
-            th.SO3.unit_quaternion_to_SO3(
-                ee_quat_mid / torch.linalg.norm(ee_quat_mid)
-            ).compose(th.SO3().exp_map(sample_vector_dist(ee_quat_range).unsqueeze(0)))
-        )
-        .to_quaternion()
-        .squeeze()
-    )
-    return th.SE3(
-        x_y_z_quaternion=torch.cat([ee_pos_target, ee_quat_target]),
+        th.SO3.unit_quaternion_to_SO3(
+            ee_quat_mid / torch.linalg.norm(ee_quat_mid)
+        ).compose(th.SO3().exp_map(ee_rot_dev))
+    ).to_quaternion()
+    ee_pose_target = th.SE3(
+        x_y_z_quaternion=torch.cat([ee_pos_target, ee_quat_target], dim=-1),
         name="ee_pose_target",
     )
 
+    return ee_pose_target
+
 
 @pytest.mark.parametrize("is_grad_enabled", [True, False])
-def test_ik_optimization(robot_model, ee_pose_target, is_grad_enabled):
+def test_ik_optimization(robot_model, batch_size, ee_pose_target, is_grad_enabled):
     """Sets up inverse kinematics as an optimization problem that uses forward kinematics"""
-    # Initialize variables (joint states)
-    theta = th.Vector(NUM_DOFS, name="theta")
-
     # Define cost (distance between desired and current ee pose)
     def ee_pose_err_fn(optim_vars, aux_vars):
-        (theta,) = optim_vars
+        theta = torch.cat([var.data for var in optim_vars], dim=0)
         (ee_pose_target,) = aux_vars
 
-        ee_pose = robot_model.forward_kinematics(theta.data)[EE_NAME]
-        err = ee_pose_target.local(ee_pose)
+        ee_pose = robot_model.forward_kinematics(theta)[EE_NAME]
+        pose_err = ee_pose_target.local(ee_pose)
 
-        return err
+        return pose_err
 
     # Set up optimization
-    optim_vars = (theta,)
+    optim_vars = [th.Vector(NUM_DOFS, name=f"theta_{i}") for i in range(batch_size)]
     aux_vars = (ee_pose_target,)
 
     cost_function = th.AutoDiffCostFunction(
@@ -79,16 +77,15 @@ def test_ik_optimization(robot_model, ee_pose_target, is_grad_enabled):
     objective.add(cost_function)
     optimizer = th.LevenbergMarquardt(
         objective,
-        max_iterations=15,
+        max_iterations=25,
         step_size=0.5,
     )
     theseus_optim = th.TheseusLayer(optimizer)
 
     # Optimize
-    theseus_inputs = {
-        "ee_pose_target": ee_pose_target.data,
-        "theta": torch.Tensor(HOME_POSE).unsqueeze(0),
-    }
+    theseus_inputs = {f"theta_{i}": HOME_POSE.unsqueeze(0) for i in range(batch_size)}
+    theseus_inputs["ee_pose_target"] = ee_pose_target.data
+
     with torch.set_grad_enabled(is_grad_enabled):
         updated_inputs, info = theseus_optim.forward(
             theseus_inputs,
@@ -101,4 +98,4 @@ def test_ik_optimization(robot_model, ee_pose_target, is_grad_enabled):
         )
 
     # Check result
-    assert info.last_err < 1e-3
+    assert torch.allclose(info.best_err, torch.zeros(batch_size), atol=1e-2)

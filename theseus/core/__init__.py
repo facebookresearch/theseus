debaad42fd32fb4292ea376a7a400141178599cf
Taosha Fan <fantaosha@gmail.com>
2022-04-03 15:29:30 -0700

    update __init__.py

--- theseus/core/__init__.py
+++ theseus/core/__init__.py
@@ -5,6 +5,6 @@
 
 from .cost_function import AutoDiffCostFunction, CostFunction, ErrFnType
 from .cost_weight import CostWeight, DiagonalCostWeight, ScaleCostWeight
-from .loss_function import HuberLoss, RobustLoss, TrivialLoss, WelschLoss
+from .loss_function import HuberLoss, LossFunction, RobustLoss, TrivialLoss, WelschLoss
 from .objective import Objective
 from .variable import Variable

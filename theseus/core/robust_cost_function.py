52a1ff1b7af2d46070665e3b2bd7c93063a0cf77
Taosha Fan <fantaosha@gmail.com>
2022-04-02 20:54:41 -0700

    update robust cost function

--- theseus/core/robust_cost_function.py
+++ theseus/core/robust_cost_function.py
@@ -3,28 +3,30 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import torch
 
-import theseus as th
-
+from ..geometry import Vector
+from .cost_function import CostFunction
 from .robust_loss import RobustLoss
 
 
-class RobustCostFunction(th.CostFunction):
+class RobustCostFunction(CostFunction):
     def __init__(
         self,
-        loss_function: RobustLoss,
-        log_loss_radius: th.Vector,
-        cost_function: th.CostFunction,
+        robust_loss: RobustLoss,
+        log_loss_radius: Vector,
+        cost_function: CostFunction,
+        name: Optional[str] = None,
     ):
         if isinstance(cost_function, RobustCostFunction):
             raise ValueError(
                 "{} is alreay a robust cost function.".format(cost_function.name)
             )
 
-        self.loss_function = loss_function
+        super().__init__(cost_weight=cost_function.weight, name=name)
+        self.robust_loss = robust_loss
         self.cost_function = cost_function
         self.log_loss_radius = log_loss_radius
         self._optim_vars_attr_names = self.cost_function._optim_vars_attr_names
@@ -43,19 +45,34 @@ class RobustCostFunction(th.CostFunction):
     def weighted_jacobians_error(self) -> Tuple[List[torch.Tensor], torch.Tensor]:
         return self.cost_function.weighted_jacobians_error()
 
+    def dim(self) -> int:
+        return self.cost_function.dim()
+
     def function_value(self) -> torch.Tensor:
         weighted_error = self.weighted_error()
         squared_norm = torch.sum(weighted_error**2, dim=1, keepdim=True)
         loss_radius = torch.exp(self.log_loss_radius.data)
-        return self.loss_function.evaluate(squared_norm, loss_radius)
+        return self.robust_loss.evaluate(squared_norm, loss_radius)
 
     def reweighted_jacobians_error(self) -> Tuple[List[torch.Tensor], torch.Tensor]:
         weighted_jacobians, weighted_error = self.weighted_jacobians_error()
         squared_norm = torch.sum(weighted_error**2, dim=1, keepdim=True)
         loss_radius = torch.exp(self.log_loss_radius.data)
-        rescale = self.loss_function.linearize(squared_norm, loss_radius).sqrt()
+        rescale = self.robust_loss.linearize(squared_norm, loss_radius).sqrt()
 
         return [
             (rescale * jacobian.view(jacobian.shape[0], -1)).view(jacobian.shape[0])
             for jacobian in weighted_jacobians
         ], rescale * weighted_error
+
+    def to(self, *args, **kwargs):
+        super().to(*args, **kwargs)
+        self.cost_function.to(*args, **kwargs)
+
+    def _copy_impl(self, new_name: Optional[str] = None) -> "RobustCostFunction":
+        return RobustCostFunction(
+            robust_loss=self.robust_loss,
+            log_loss_radius=self.log_loss_radius.copy(),
+            cost_function=self.cost_function.copy(),
+            name=new_name,
+        )

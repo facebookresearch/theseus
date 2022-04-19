Fixed small bug in memory computation for backward pass.

theseus/utils/examples/tactile_pose_estimation/trainer.py
@@ -151,7 +151,7 @@ class TactilePushingTrainer:
         end_event.record()
         torch.cuda.synchronize()
         backward_time = start_event.elapsed_time(end_event)
-        backward_mem = torch.cuda.max_memory_allocated() / 1025 / 1024
+        backward_mem = torch.cuda.max_memory_allocated() / 1048576
         logger.info(f"Backward pass took {backward_time} ms.")
         logger.info(f"Backward pass used {backward_mem} MBs.")
 
@@ -236,7 +236,7 @@ class TactilePushingTrainer:
             end_event.record()
             torch.cuda.synchronize()
             forward_time = start_event.elapsed_time(end_event)
-            forward_mem = torch.cuda.max_memory_allocated() / 1024 / 1024
+            forward_mem = torch.cuda.max_memory_allocated() / 1048576
             logger.info(f"Forward pass took {forward_time} ms.")
             logger.info(f"Forward pass used {forward_mem} MBs.")
 
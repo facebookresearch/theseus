7f96736e686b3ce0a6b0b59c920eef65fec233bb
joeaortiz <joeaortiz16@gmail.com>
2022-05-09 11:27:30 +0100

    split dataset, add test loop

--- examples/homography_estimation.py
+++ examples/homography_estimation.py
@@ -34,7 +34,7 @@ BACKWARD_MODE = {
 
 
 class HomographyDataset(Dataset):
-    def __init__(self, img_dir, imgH, imgW, photo_aug=True):
+    def __init__(self, img_dir, imgH, imgW, photo_aug=True, train=True):
         self.img_dir = img_dir
         self.imgH = imgH
         self.imgW = imgW
@@ -53,6 +53,13 @@ class HomographyDataset(Dataset):
         self.photo_aug = photo_aug
         if self.photo_aug:
             self.rpa = RandomPhotoAug()
+
+        # train test split
+        self.img_paths.sort()
+        if train:
+            self.img_paths = self.img_paths[:-100]
+        else:
+            self.img_paths = self.img_paths[-100:]
 
     def __len__(self):
         return len(self.img_paths)
@@ -151,7 +158,9 @@ def warp_perspective_norm(H, img):
     return img2
 
 
-def homography_error_fn(optim_vars: List[th.Manifold], aux_vars: List[th.Variable]):
+def homography_error_fn(
+    optim_vars: List[th.Manifold], aux_vars: List[th.Variable], pool_size: int
+):
     H_1_2 = optim_vars[0]
     img1, img2 = aux_vars
 
@@ -162,8 +171,7 @@ def homography_error_fn(optim_vars: List[th.Manifold], aux_vars: List[th.Variabl
     mask = warp_perspective_norm(H_1_2_mat, torch.ones_like(img1.data))
     mask = mask > 0.9
     loss = (loss * mask).mean(dim=1)
-    pool = torch.nn.AvgPool2d(6, stride=6)
-    loss = pool(loss)
+    loss = torch.nn.functional.avg_pool2d(loss, pool_size, stride=pool_size)
     loss = loss.reshape(loss.shape[0], -1)
     return loss
 
@@ -184,7 +192,7 @@ def torch2cv2(img):
     return out
 
 
-def viz_warp(log_dir, img1, img2, img1_w, iteration, err):
+def make_viz(img1, img2, img1_w, iteration, err):
     img_diff = torch2cv2(torch.abs(img1_w - img2))
     img1 = torch2cv2(img1)
     img2 = torch2cv2(img2)
@@ -192,13 +200,56 @@ def viz_warp(log_dir, img1, img2, img1_w, iteration, err):
     img1 = put_text(img1, "image I")
     img2 = put_text(img2, "image I'")
     img1_w = put_text(img1_w, "I warped to I'")
+    img1_w = put_text(img1_w, "iter: %05d, loss: %.3f" % (iteration, err), top=False)
     img_diff = put_text(img_diff, "L2 diff")
     out = np.concatenate([img1, img2, img1_w, img_diff], axis=1)
-    out = put_text(out, "iter: %05d, loss: %.3f" % (iteration, err), top=False)
+    return out
+
+
+def viz_warp(log_dir, img1, img2, img1_w, iteration, err):
+    out = make_viz(img1, img2, img1_w, iteration, err)
     path = os.path.join(log_dir, "out_%05d.png" % iteration)
     cv2.imwrite(path, out)
-    # cv2.imshow("i", out)
-    # cv2.waitKey(0)
+
+
+def setup_homgraphy_layer(cfg, device, batch_size, imgH, imgW, channels):
+
+    objective = th.Objective()
+
+    H_init = torch.zeros(batch_size, 8)
+    H_1_2 = th.Vector(data=H_init, name="H_1_2")
+
+    img_init = torch.zeros(batch_size, channels, imgH, imgW)
+    img1 = th.Variable(data=img_init, name="img1")
+    img2 = th.Variable(data=img_init, name="img2")
+
+    # loss is pooled so error dim is not too large
+    pool_size = int(imgH // 4)
+    pooled = torch.nn.functional.avg_pool2d(img_init, pool_size, stride=pool_size)
+    error_dim = pooled[0, 0].numel()
+    print(f"Pool size {pool_size}, error dim {error_dim}")
+
+    def error_fn(optim_vars: List[th.Manifold], aux_vars: List[th.Variable]):
+        return homography_error_fn(optim_vars, aux_vars, pool_size)
+
+    homography_cf = th.AutoDiffCostFunction(
+        optim_vars=[H_1_2],
+        err_fn=error_fn,
+        dim=error_dim,
+        aux_vars=[img1, img2],
+    )
+    objective.add(homography_cf)
+
+    optimizer = th.LevenbergMarquardt(
+        objective,
+        max_iterations=cfg.inner_optim.max_iters,
+        step_size=cfg.inner_optim.step_size,
+    )
+
+    theseus_layer = th.TheseusLayer(optimizer)
+    theseus_layer.to(device)
+
+    return theseus_layer
 
 
 def run(cfg):
@@ -219,117 +270,184 @@ def run(cfg):
         os.system(cmd)
 
     imgH, imgW = 160, 200
-    channels = 3
-    dataset = HomographyDataset(dataset_path, imgH, imgW)
-    batch_size = 10
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    data = next(iter(dataloader))
+    batch_size = 5
+    train_dataset = HomographyDataset(dataset_path, imgH, imgW, train=True)
+    test_dataset = HomographyDataset(dataset_path, imgH, imgW, train=False)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
     log_dir = "viz"
     os.makedirs(log_dir, exist_ok=True)
 
     device = "cuda" if torch.cuda.is_available() and cfg.use_gpu else "cpu"
 
-    if cfg.use_feats:
-        resnet = models.resnet18(pretrained=True)
-        resnet.to(device)
-        resnet_feat = torch.nn.Sequential(*list(resnet.children())[:-4])
-        imgH, imgW = 20, 25
-        channels = 128
+    resnet = models.resnet18(pretrained=True)
+    resnet.to(device)
+    resnet_feat = torch.nn.Sequential(*list(resnet.children())[:-4])
+    fixed_feat = torch.nn.Sequential(*list(resnet.children())[:-4])
+    featH, featW = 20, 25
+    feat_channels = 128
 
-        model_optimizer = torch.optim.Adam(
-            resnet_feat.parameters(), lr=cfg.outer_optim.lr
-        )
+    model_optimizer = torch.optim.Adam(resnet_feat.parameters(), lr=cfg.outer_optim.lr)
 
-        l1_loss = torch.nn.L1Loss()
+    l1_loss = torch.nn.L1Loss()
 
-    objective = th.Objective()
-
-    H_init = torch.eye(3).reshape(1, 9).repeat(batch_size, 1).to(device)
-    H_init = H_init[:, :-1]  # last element always one
-    H_1_2 = th.Vector(data=H_init, name="H_1_2")
-
-    img_init = torch.zeros(batch_size, channels, imgH, imgW)
-    img1 = th.Variable(data=img_init, name="img1")
-    img2 = th.Variable(data=img_init, name="img2")
-
-    # loss is pooled so error dim is not too large
-    error_dim = 12
-    homography_cf = th.AutoDiffCostFunction(
-        optim_vars=[H_1_2],
-        err_fn=homography_error_fn,
-        dim=error_dim,
-        aux_vars=[img1, img2],
-    )
-    objective.add(homography_cf)
-
-    optimizer = th.LevenbergMarquardt(
-        objective,
-        max_iterations=cfg.inner_optim.max_iters,
-        step_size=cfg.inner_optim.step_size,
+    layer_feat = setup_homgraphy_layer(
+        cfg,
+        device,
+        batch_size,
+        featH,
+        featW,
+        feat_channels,
     )
 
-    theseus_layer = th.TheseusLayer(optimizer)
-    theseus_layer.to(device)
+    # Training loop to refine pretrained features
 
     for epoch in range(cfg.outer_optim.num_epochs):
         epoch_losses = []
         start_time = time.time()
-        for t, data in enumerate(dataloader):
+        for t, data in enumerate(train_loader):
 
             H_1_2_gt = data["H_1_2"].to(device)
-            img1_init = data["img1"].to(device)
-            img2_init = data["img2"].to(device)
+            img1 = data["img1"].to(device)
+            img2 = data["img2"].to(device)
+            feat1 = resnet_feat(img1)
+            feat2 = resnet_feat(img2)
 
-            if cfg.use_feats:
-                img1_init = resnet_feat(img1_init)
-                img2_init = resnet_feat(img2_init)
-
+            H_init = torch.eye(3).reshape(1, 9).repeat(batch_size, 1).to(device)
+            H_init = H_init[:, :-1]
             inputs = {
                 "H_1_2": H_init,
-                "img1": img1_init,
-                "img2": img2_init,
+                "img1": feat1,
+                "img2": feat2,
             }
 
-            info = theseus_layer.forward(
+            info = layer_feat.forward(
                 inputs,
                 optimizer_kwargs={
                     "verbose": False,
                     "backward_mode": BACKWARD_MODE[cfg.inner_optim.backward_mode],
+                    # "damping": 1e-2,
                 },
             )
 
-            if cfg.use_feats:
-                # no loss on last element as set to one
-                H_1_2_gt = H_1_2_gt.reshape(-1, 9)[:, :-1]
-                loss = l1_loss(H_1_2.data, H_1_2_gt)
-                loss.backward()
-                model_optimizer.step()
-                epoch_losses.append(loss.item())
+            # no loss on last element as set to one
+            H_1_2_gt = H_1_2_gt.reshape(-1, 9)[:, :-1]
+            H_1_2 = info[0]["H_1_2"]
+            loss = l1_loss(H_1_2.data, H_1_2_gt)
+            loss.backward()
+            model_optimizer.step()
+            epoch_losses.append(loss.item())
 
+            if t % 10 == 0:
                 print(f"Step {t}. Loss {loss.item():.4f}")
-
-            if cfg.vis_training and cfg.use_feats is False:
-                ones = torch.ones(
-                    H_1_2.shape[0], 1, device=H_1_2.device, dtype=H_1_2.dtype
-                )
-                H_1_2_mat = torch.cat((H_1_2.data, ones), dim=1).reshape(-1, 3, 3)
-                img1_dst = warp_perspective_norm(H_1_2_mat, img1.data)
-                for i in range(batch_size):
-                    viz_warp(
-                        log_dir,
-                        img1.data[i],
-                        img2.data[i],
-                        img1_dst[i],
-                        info[1].converged_iter[i].item(),
-                        float(objective.error()[i].item()),
-                    )
 
         epoch_time = time.time() - start_time
         print(
             f"******* Epoch {epoch}. Average loss: {np.mean(epoch_losses):.4f}. "
             f"Epoch time {epoch_time}*******"
         )
+
+    # Test loop
+
+    print("\n**** Test dataset evaluation ****")
+
+    layer_pho = setup_homgraphy_layer(
+        cfg,
+        device,
+        batch_size,
+        imgH,
+        imgW,
+        3,
+    )
+
+    with torch.no_grad():
+        for t, data in enumerate(test_loader):
+
+            img1 = data["img1"].to(device)
+            img2 = data["img2"].to(device)
+            feat1 = resnet_feat(img1)
+            feat2 = resnet_feat(img2)
+            fixed_feat1 = fixed_feat(img1)
+            fixed_feat2 = fixed_feat(img2)
+
+            H_init = torch.eye(3).reshape(1, 9).repeat(batch_size, 1).to(device)
+            H_init = H_init[:, :-1]
+
+            inputs_img = {
+                "H_1_2": H_init,
+                "img1": img1,
+                "img2": img2,
+            }
+
+            inputs_feat = {
+                "H_1_2": H_init,
+                "img1": feat1,
+                "img2": feat2,
+            }
+
+            inputs_fixed_feat = {
+                "H_1_2": H_init,
+                "img1": fixed_feat1,
+                "img2": fixed_feat2,
+            }
+
+            layer_feat.optimizer.params.max_iterations = cfg.test_max_iters
+            layer_pho.optimizer.params.max_iterations = cfg.test_max_iters
+
+            info_opt = layer_feat.forward(inputs_feat)
+            H_opt = info_opt[0]["H_1_2"]
+            info_fix = layer_feat.forward(inputs_fixed_feat)
+            H_fix = info_fix[0]["H_1_2"]
+            info_pho = layer_pho.forward(inputs_img)
+            H_pho = info_pho[0]["H_1_2"]
+
+            # Compute evalution metric, for now photometric loss
+            img1_var = th.Variable(data=img1, name="img1")
+            img2_var = th.Variable(data=img2, name="img2")
+
+            loss_opt = homography_error_fn([H_opt], [img1_var, img2_var], pool_size=1)
+            loss_fix = homography_error_fn([H_fix], [img1_var, img2_var], pool_size=1)
+            loss_pho = homography_error_fn([H_pho], [img1_var, img2_var], pool_size=1)
+            loss_opt = loss_opt.mean(dim=1)
+            loss_fix = loss_fix.mean(dim=1)
+            loss_pho = loss_pho.mean(dim=1)
+
+            print(
+                f"Photometric errors: optimised feats {loss_opt.mean():.3f}, "
+                f"fixed feats {loss_fix.mean():.3f}, photometric {loss_pho.mean():.3f}"
+            )
+
+            if cfg.vis_res:
+                Hs = [H_opt, H_fix, H_pho]
+                losses = [loss_opt, loss_fix, loss_pho]
+                infos = [info_opt, info_fix, info_pho]
+                labels = [
+                    "Optimised featuremetric",
+                    "Pretrained featuremetric",
+                    "Photometric",
+                ]
+                img1_dsts = []
+                for H in Hs:
+                    ones = torch.ones(H.shape[0], 1, device=H.device, dtype=H.dtype)
+                    H_1_2_mat = torch.cat((H.data, ones), dim=1).reshape(-1, 3, 3)
+                    img1_dsts.append(warp_perspective_norm(H_1_2_mat, img1))
+
+                for i in range(batch_size):
+                    imgs = []
+                    for j in range(3):
+                        viz = make_viz(
+                            img1[i],
+                            img2[i],
+                            img1_dsts[j][i],
+                            infos[j][1].converged_iter[i],
+                            float(losses[j][i].item()),
+                        )
+                        viz = put_text(viz, labels[j], top=False)
+                        imgs.append(viz)
+                    imgs = np.vstack((imgs))
+                    cv2.imshow("viz", imgs)
+                    cv2.waitKey(0)
 
 
 @hydra.main(config_path="./configs/", config_name="homography_estimation")

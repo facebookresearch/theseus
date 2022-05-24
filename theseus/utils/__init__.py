a5a14963c7e984d095e549212649c419b07e81b3
Luis Pineda <luisen.p@gmail.com>
2022-05-24 12:40:32 -0700

    Removed copyright notice.

--- theseus/utils/__init__.py
+++ theseus/utils/__init__.py
@@ -1,7 +1,2 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
 from .sparse_matrix_utils import random_sparse_binary_matrix
 from .utils import build_mlp, gather_from_rows_cols, numeric_jacobian
--- theseus/utils/examples/bundle_adjustment/__init__.py
+++ theseus/utils/examples/bundle_adjustment/__init__.py
@@ -1,7 +1,2 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
 from .data import BundleAdjustmentDataset, Camera, ba_histogram
 from .reprojection_error import Reprojection

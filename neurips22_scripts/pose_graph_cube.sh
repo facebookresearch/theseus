889e9fd834ab3b2aaff19ab77236750db1040be1
Taosha Fan <fantaosha@gmail.com>
2022-05-26 09:17:56 -0700

    update scripts

--- neurips22_scripts/pose_graph_cube.sh
+++ neurips22_scripts/pose_graph_cube.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# This script can be used to generate the results in Figure 2
+# This script can be used to generate the results in Figure 3
 # Options that can be changed for values used in the paper:
-#  inner_optim.solver: {dense, sparse}
 #  solver_device: {cuda, cpu}
-#  batch_size: {8, 16, 32, 64, 128, 256}
+#  batch_size: {1, 2, 4, 8, 16, 32, 64, 128, 256}
 #  num_poses: {128, 256, 512, 1024, 2048, 4096}
 
-python examples/pose_graph_synthetic.py --multirun \
+python examples/pose_graph_cube.py --multirun \
        hydra/launcher=submitit_slurm \
        hydra.launcher.partition=learnlab \
        hydra.launcher.timeout_min=4320 \
@@ -15,9 +14,7 @@ python examples/pose_graph_synthetic.py --multirun \
        hydra.launcher.gpus_per_node=1 \
        hydra.launcher.mem_gb=128 \
        hydra.launcher.constraint=volta32gb \
-       inner_optim.solver=sparse \
-       loop_closure_ratio=0.2 \
-       solver_device=cuda \
+       solver_device=cpu \
+       device=cpu \
        dataset_size=256 \
-       batch_size=128 \
        num_poses=256

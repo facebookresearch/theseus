2277659de039df8f79610da0fda770807bcf9b11
Taosha Fan <fantaosha@gmail.com>
2022-06-27 08:26:11 -0700

    update constants for Lie groups

--- theseus/constants.py
+++ theseus/constants.py
@@ -7,11 +7,31 @@ import math
 
 import torch
 
-TEST_EPS = 3e-7
+TEST_EPS = 5e-7
 EPS = 1e-10
 PI = math.pi
 
 _SE2_NEAR_ZERO_EPS = {torch.float32: 3e-2, torch.float64: 5e-7}
+
+_SO3_NEAR_PI_EPS = {
+    torch.float32: 1e-2,
+    torch.float64: 1e-7,
+}
+
+_SO3_NEAR_ZERO_EPS = {
+    torch.float32: 1e-2,
+    torch.float64: 5e-3,
+}
+
+_SO3_MATRIX_EPS = {
+    torch.float32: 1e-5,
+    torch.float64: 5e-7,
+}
+
+_SO3_QUATERNION_EPS = {
+    torch.float32: 2e-4,
+    torch.float64: 5e-7,
+}
 
 _SE3_NEAR_PI_EPS = {
     torch.float32: 1e-2,

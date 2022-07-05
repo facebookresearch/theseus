fixed a bug for quaternion check

theseus/geometry/so3.py
@@ -145,12 +145,14 @@ class SO3(LieGroup):
         if quaternion.ndim != 2 or quaternion.shape[1] != 4:
             raise ValueError("Quaternions can only be 4-D vectors.")
 
+        QUANTERNION_EPS = theseus.constants._SO3_QUATERNION_EPS[quaternion.dtype]
+
         if quaternion.dtype != torch.float64:
             quaternion = quaternion.double()
 
         if (
             torch.linalg.norm(quaternion, dim=1) - 1
-        ).abs().max().item() >= theseus.constants._SO3_QUATERNION_EPS[quaternion.dtype]:
+        ).abs().max().item() >= QUANTERNION_EPS:
             raise ValueError("Not unit quaternions.")
 
     @staticmethod
4473a01cfecb1733087976c2a41172f9a708267c
Luis Pineda <luisen.p@gmail.com>
2022-07-27 18:43:55 -0700

    Fixed broken test and added missing copyright headers.

--- theseus/embodied/collision/tests/test_eff_obj_contact.py
+++ theseus/embodied/collision/tests/test_eff_obj_contact.py
@@ -159,40 +159,43 @@ def test_eff_obj_interesect_errors():
 def test_eff_obj_variable_type():
     rng = torch.Generator()
     rng.manual_seed(0)
-    for batch_size in [1, 10, 100]:
-        obj = create_random_se2(batch_size, rng)
-        eff = create_random_se2(batch_size, rng)
-        origin = random_origin(batch_size)
-        sdf_data = random_sdf_data(batch_size, 10, 10)
-        cell_size = random_scalar(batch_size)
-        eff_radius = random_scalar(batch_size)
-        cost_weight = th.ScaleCostWeight(1.0)
-        cost_function = th.eb.EffectorObjectContactPlanar(
-            obj, eff, origin, sdf_data, cell_size, eff_radius, cost_weight
-        )
+    for _ in range(10):
+        for batch_size in [1, 10, 100]:
+            obj = create_random_se2(batch_size, rng)
+            eff = create_random_se2(batch_size, rng)
+            origin = random_origin(batch_size)
+            sdf_data = random_sdf_data(batch_size, 10, 10)
+            cell_size = random_scalar(batch_size)
+            eff_radius = random_scalar(batch_size)
+            cost_weight = th.ScaleCostWeight(1.0)
+            cost_function = th.eb.EffectorObjectContactPlanar(
+                obj, eff, origin, sdf_data, cell_size, eff_radius, cost_weight
+            )
 
-        assert isinstance(cost_function.eff_radius, th.Variable)
-        if isinstance(eff_radius, th.Variable):
-            assert cost_function.eff_radius is eff_radius
-        else:
-            assert torch.allclose(cost_function.eff_radius.tensor, eff_radius)
+            assert isinstance(cost_function.eff_radius, th.Variable)
+            if isinstance(eff_radius, th.Variable):
+                assert cost_function.eff_radius is eff_radius
+            else:
+                assert np.allclose(
+                    cost_function.eff_radius.tensor.cpu().numpy(), eff_radius
+                )
 
-        eff_radius_t = torch.rand(batch_size, 1).double()
+            eff_radius_t = torch.rand(batch_size, 1).double()
 
-        cost_function = th.eb.EffectorObjectContactPlanar(
-            obj, eff, origin, sdf_data, cell_size, eff_radius_t, cost_weight
-        )
+            cost_function = th.eb.EffectorObjectContactPlanar(
+                obj, eff, origin, sdf_data, cell_size, eff_radius_t, cost_weight
+            )
 
-        assert isinstance(cost_function.eff_radius, th.Variable)
-        assert np.allclose(cost_function.eff_radius.tensor, eff_radius_t)
-        assert len(cost_function.eff_radius.shape) == 2
+            assert isinstance(cost_function.eff_radius, th.Variable)
+            assert np.allclose(cost_function.eff_radius.tensor, eff_radius_t)
+            assert len(cost_function.eff_radius.shape) == 2
 
-        eff_radius_f = torch.rand(1)
+            eff_radius_f = torch.rand(1)
 
-        cost_function = th.eb.EffectorObjectContactPlanar(
-            obj, eff, origin, sdf_data, cell_size, eff_radius_f, cost_weight
-        )
+            cost_function = th.eb.EffectorObjectContactPlanar(
+                obj, eff, origin, sdf_data, cell_size, eff_radius_f, cost_weight
+            )
 
-        assert isinstance(cost_function.eff_radius, th.Variable)
-        assert np.allclose(cost_function.eff_radius.tensor.item(), eff_radius_f)
-        assert len(cost_function.eff_radius.shape) == 2
+            assert isinstance(cost_function.eff_radius, th.Variable)
+            assert np.allclose(cost_function.eff_radius.tensor.item(), eff_radius_f)
+            assert len(cost_function.eff_radius.shape) == 2

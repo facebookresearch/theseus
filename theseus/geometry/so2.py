08ca20822fb27e077268a4044d5589819bc46d59
Taosha Fan <fantaosha@gmail.com>
2022-08-11 11:20:09 -0700

    fixed a bug in SO2._rotate_from_cos_sin

--- theseus/geometry/so2.py
+++ theseus/geometry/so2.py
@@ -257,9 +257,7 @@ class SO2(LieGroup):
         else:
             point_tensor = point.tensor
         px, py = point_tensor[:, 0], point_tensor[:, 1]
-        new_point_tensor = torch.empty(
-            batch_size, 2, device=cosine.device, dtype=cosine.dtype
-        )
+        new_point_tensor = point_tensor.new_empty(batch_size, 2)
         new_point_tensor[:, 0] = cosine * px - sine * py
         new_point_tensor[:, 1] = sine * px + cosine * py
         return Point2(tensor=new_point_tensor)

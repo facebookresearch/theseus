06abb122c12efbc6baffb78a1fecdd8d72a7b7be
Luis Pineda <luisen.p@gmail.com>
2022-08-17 08:15:20 -0700

    Fixed dockerfile generation to work with other CUDA versions.

--- build_scripts/build_wheel.sh
+++ build_scripts/build_wheel.sh
@@ -1,18 +1,18 @@
 #bin/bash
 
-# Ensure that 1 argument (ROOT_DIR) is provided
+# Ensure that 2 arguments (ROOT_DIR, TAG) are provided
 die () {
     echo >&2 "$@"
     exit 1
 }
-[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+[ "$#" -eq 2 ] || die "2 arguments required, $# provided"
 ROOT_DIR=$1
-
+TAG=$2
 # This needs to match the version in main on Github
 THESEUS_VERSION=$(head theseus/__init__.py -n 6 | tail -n 1 | awk -F'"' '{print $2}')
 
 for PYTHON_VERSION in 3.9; do
-    for CUDA_VERSION in 10.2; do
+    for CUDA_VERSION in 10.2 11.6; do
 
         CUDA_SUFFIX=$(echo ${CUDA_VERSION} | sed 's/[.]//g')
 
@@ -20,19 +20,21 @@ for PYTHON_VERSION in 3.9; do
         DOCKER_DIR=${ROOT_DIR}/docker_${PYTHON_VERSION}
         mkdir -p ${DOCKER_DIR}
         echo """FROM pytorch/manylinux-cuda${CUDA_SUFFIX}
-
         ENV CONDA_DIR /opt/conda
         RUN wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
             /bin/bash ~/miniconda.sh -b -p /opt/conda
 
-        ENV PATH=\$CONDA_DIR/bin:\$PATH
+        ENV PATH \$CONDA_DIR/bin:\$PATH
         RUN conda create --name theseus python=${PYTHON_VERSION}
         RUN source activate theseus
-        RUN pip install torch
+        RUN which python
+        ENV CUDA_HOME /usr/local/cuda-${CUDA_VERSION}
+        RUN pip install torch --extra-index-url https://download.pytorch.org/whl/cu${CUDA_SUFFIX}
         RUN conda install -c conda-forge suitesparse
         RUN pip install build wheel
         RUN git clone https://github.com/facebookresearch/theseus.git
         WORKDIR theseus
+        RUN git checkout ${TAG} -b tmp_build
         CMD python3 -m build --no-isolation
         """ > ${DOCKER_DIR}/Dockerfile
 
@@ -40,7 +42,7 @@ for PYTHON_VERSION in 3.9; do
         cd ${DOCKER_DIR}
         echo $(pwd)
         DOCKER_NAME=theseus_${PYTHON_VERSION}
-        sudo docker build -t ${DOCKER_NAME}_img .
+        sudo docker build -t "${DOCKER_NAME}_img" .
         sudo docker run --gpus all --name ${DOCKER_NAME} ${DOCKER_NAME}_img
 
         # # Copy the wheel to host
@@ -50,6 +52,6 @@ for PYTHON_VERSION in 3.9; do
         sudo docker cp "${DOCKER_NAME}:${DOCKER_WHL}" ${DOCKER_DIR}/${HOST_WHL}
 
         sudo docker rm ${DOCKER_NAME}
-        sudo docker image rm ${DOCKER_NAME}_img
+        sudo docker image rm "${DOCKER_NAME}_img"
     done
 done

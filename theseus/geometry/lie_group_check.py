85495aba3778aaf869c02e511ea67fba810bbf0d
Taosha Fan <fantaosha@gmail.com>
2022-09-07 11:39:46 -0700

    rename lie_group_tensor to lie_group

---
+++ theseus/geometry/lie_group_check.py
@@ -0,0 +1,52 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import threading
+from typing import Any
+
+
+class _LieGroupContext:
+    contexts = threading.local()
+
+    @classmethod
+    def get_context(cls):
+        if not hasattr(cls.contexts, "check_lie_group"):
+            cls.contexts.check_lie_group = False
+        return cls.contexts.check_lie_group
+
+    @classmethod
+    def set_context(cls, check_lie_group: bool):
+        cls.contexts.check_lie_group = check_lie_group
+
+
+class set_lie_group_check_enabled:
+    def __init__(self, mode: bool) -> None:
+        self.prev = _LieGroupContext.get_context()
+        _LieGroupContext.set_context(mode)
+
+    def __enter__(self) -> None:
+        pass
+
+    def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> None:
+        _LieGroupContext.set_context(self.prev)
+
+
+class enable_lie_group_check(_LieGroupContext):
+    def __enter__(self) -> None:
+        self.prev = _LieGroupContext.get_context()
+        _LieGroupContext.set_context(True)
+
+    def __exit__(self, typ, value, traceback) -> None:
+        _LieGroupContext.set_context(self.prev)
+
+
+class no_lie_group_check(_LieGroupContext):
+    def __enter__(self):
+        self.prev = super().get_context()
+        _LieGroupContext.set_context(False)
+        return self
+
+    def __exit__(self, typ, value, traceback):
+        _LieGroupContext.set_context(self.prev)

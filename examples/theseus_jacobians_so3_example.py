bdbf0fe6ffdb660aa27f9912ba4c888e4b345129
Taosha Fan <fantaosha@gmail.com>
2022-10-10 17:52:37 -0700

    add more benchmark cases

--- examples/theseus_jacobians_so3_example.py
+++ examples/theseus_jacobians_so3_example.py
@@ -1,7 +1,10 @@
+from math import tan
+
 import numpy as np
 import torch
 
 import theseus as th
+from theseus.geometry.lie_group_check import no_lie_group_check
 from theseus.utils import numeric_jacobian
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -14,7 +17,7 @@ log = []
 np.set_printoptions(precision=3)
 
 
-def test_theseus_jacobian(batch_size, method="analytic"):
+def test_theseus_rotate_jacobian(batch_size, method="analytic"):
     torch.cuda.empty_cache()
     try:  # this fails for CPU, just ignore
         torch.cuda.get_device_capability(device)
@@ -60,7 +63,7 @@ def test_theseus_jacobian(batch_size, method="analytic"):
             return (rot_.rotate(x_) + trans_).tensor
 
         autograd_mode = "dense" if "dense" in method else "vmap"
-        cost = th.AutoDiffCostFunction(
+        error = th.AutoDiffCostFunction(
             [input],
             cost_fn,
             3,
@@ -69,7 +72,136 @@ def test_theseus_jacobian(batch_size, method="analytic"):
             autograd_vectorize=True,
             autograd_strict=False,
         )
-        J = cost.jacobians()[0][0]  # jacobians returns ([jac], cost)
+        J, y = error.jacobians()  # jacobians returns ([jac], cost)
+        J = J[0][0]
+
+    end_event.record()
+    torch.cuda.synchronize()
+    return start_event.elapsed_time(end_event)
+
+
+def test_theseus_exp_map_jacobian(batch_size, method="analytic"):
+    torch.cuda.empty_cache()
+    try:  # this fails for CPU, just ignore
+        torch.cuda.get_device_capability(device)
+    except Exception:
+        pass
+
+    tangent_vector = th.Vector.rand(batch_size, 3)
+    start_event = torch.cuda.Event(enable_timing=True)
+    end_event = torch.cuda.Event(enable_timing=True)
+    start_event.record()
+    if method == "analytic":
+        Jexp = []  # will store dexp/dR and dexp/dx
+        th.SO3.exp_map(tangent_vector=tangent_vector.tensor, jacobians=Jexp)
+        J = Jexp[0]
+    elif "autograd" in method:
+
+        def err_fn(optim_vars, aux_vars):
+            # no_lie_group_check() ignores the validness check of SO3, e.g., det(R)=1, R^T@R=I.
+            with no_lie_group_check():
+                return th.SO3.exp_map(tangent_vector=optim_vars[0].tensor).tensor.view(
+                    -1, 9
+                )
+
+        autograd_mode = "dense" if "dense" in method else "vmap"
+        error = th.AutoDiffCostFunction(
+            [tangent_vector],
+            err_fn,
+            9,
+            aux_vars=[],
+            autograd_mode=autograd_mode,
+            autograd_vectorize=True,
+            autograd_strict=False,
+        )
+        Jexp, Rvec = error.jacobians()  # jacobians returns ([jac], R)
+        R = Rvec.view(-1, 3, 3)
+        J = Jexp[0]
+
+    end_event.record()
+    torch.cuda.synchronize()
+    return start_event.elapsed_time(end_event)
+
+
+def test_theseus_log_map_jacobian(batch_size, method="analytic"):
+    torch.cuda.empty_cache()
+    try:  # this fails for CPU, just ignore
+        torch.cuda.get_device_capability(device)
+    except Exception:
+        pass
+
+    R = th.SO3.rand(batch_size, device=device, dtype=torch.float32)
+
+    start_event = torch.cuda.Event(enable_timing=True)
+    end_event = torch.cuda.Event(enable_timing=True)
+    start_event.record()
+    if method == "analytic":
+        Jlog = []  # will store dlog/dtanget_vector
+        tangent_vector = R.log_map(jacobians=Jlog)
+        J = Jlog[0]
+    elif "autograd" in method:
+
+        def err_fn(optim_vars, aux_vars):
+            # no_lie_group_check() ignores the validness check of SO3, e.g., det(R)=1, R^T@R=I.
+            with no_lie_group_check():
+                return optim_vars[0].log_map()
+
+        autograd_mode = "dense" if "dense" in method else "vmap"
+        error = th.AutoDiffCostFunction(
+            [R],
+            err_fn,
+            3,
+            aux_vars=[],
+            autograd_mode=autograd_mode,
+            autograd_vectorize=True,
+            autograd_strict=False,
+        )
+        Jlog, tangent_vector = error.jacobians()  # jacobians returns ([jac], cost)
+        J = Jlog[0]
+
+    end_event.record()
+    torch.cuda.synchronize()
+    return start_event.elapsed_time(end_event)
+
+
+def test_theseus_compose_jacobian(batch_size, method="analytic"):
+    torch.cuda.empty_cache()
+    try:  # this fails for CPU, just ignore
+        torch.cuda.get_device_capability(device)
+    except Exception:
+        pass
+
+    R0 = th.SO3.rand(batch_size, device=device, dtype=torch.float32)
+    R1 = th.SO3.rand(batch_size, device=device, dtype=torch.float32)
+
+    start_event = torch.cuda.Event(enable_timing=True)
+    end_event = torch.cuda.Event(enable_timing=True)
+    start_event.record()
+    if method == "analytic":
+        Jcomp = []  # will store dcomp/dR0 and dcomp/dR1
+        comp = R0.compose(R1, jacobians=Jcomp)
+        J0 = Jcomp[0]
+        J1 = Jcomp[1]
+    elif "autograd" in method:
+
+        def err_fn(optim_vars, aux_vars):
+            # no_lie_group_check() ignores the validness check of SO3, e.g., det(R)=1, R^T@R=I.
+            with no_lie_group_check():
+                return optim_vars[0].compose(optim_vars[1]).tensor.view(-1, 9)
+
+        autograd_mode = "dense" if "dense" in method else "vmap"
+        error = th.AutoDiffCostFunction(
+            [R0, R1],
+            err_fn,
+            9,
+            aux_vars=[],
+            autograd_mode=autograd_mode,
+            autograd_vectorize=True,
+            autograd_strict=False,
+        )
+        Jcomp, comp = error.jacobians()  # jacobians returns ([jac], cost)
+        J0 = Jcomp[0]
+        J1 = Jcomp[1]
 
     end_event.record()
     torch.cuda.synchronize()
@@ -80,6 +212,8 @@ if __name__ == "__main__":
     # Can make batch size even larger (100,000 akes <400MBs of GPU memory)
     batch_sizes = [1, 10, 100, 1000, 9000, 100000]
 
+    print("==============================================")
+    print("               SO3.rotate()")
     for method in ["numeric", "analytic", "autograd_vmap", "autograd_dense"]:
         log = []
         for i in batch_sizes:
@@ -92,7 +226,123 @@ if __name__ == "__main__":
                 if method == "autograd_dense" and batch_sizes[i] > 10000:
                     log[i].append(-1)
                 else:
-                    log[i].append(test_theseus_jacobian(batch_sizes[i], method=method))
+                    log[i].append(
+                        test_theseus_rotate_jacobian(batch_sizes[i], method=method)
+                    )
+
+        for i in log:
+            i.sort()
+
+        np_log = np.array(log, dtype=np.float64)
+        # remove top 20% and last 20%, to filt noise
+        np_log = np_log[:, int(itrs * 0.2) : int(itrs * 0.8)]
+
+        mean = np.mean(np_log, axis=1)
+        std = np.std(np_log, axis=1)
+
+        print("----------------------------------------------")
+        jac_type = "Dense" if "dense" in method else "Sparse"
+        print(f"{jac_type} jacobian calculation method: {method}")
+        if method == "autograd_dense":
+            print(f"(Up to batch size {batch_sizes[-2]})")
+        print(f"Mean = {mean}")
+        print(f"Std = {std}")
+        print("----------------------------------------------")
+
+    print("\n")
+    print("==============================================")
+    print("              SO3.exp_map()")
+    for method in ["analytic", "autograd_vmap", "autograd_dense"]:
+        log = []
+        for i in batch_sizes:
+            log.append([])
+
+        itrs = 100
+
+        for itr in range(itrs):
+            for i in range(len(batch_sizes)):
+                if method == "autograd_dense" and batch_sizes[i] > 100:
+                    log[i].append(-1)
+                else:
+                    log[i].append(
+                        test_theseus_exp_map_jacobian(batch_sizes[i], method=method)
+                    )
+
+        for i in log:
+            i.sort()
+
+        np_log = np.array(log, dtype=np.float64)
+        # remove top 20% and last 20%, to filt noise
+        np_log = np_log[:, int(itrs * 0.2) : int(itrs * 0.8)]
+
+        mean = np.mean(np_log, axis=1)
+        std = np.std(np_log, axis=1)
+
+        print("----------------------------------------------")
+        jac_type = "Dense" if "dense" in method else "Sparse"
+        print(f"{jac_type} jacobian calculation method: {method}")
+        if method == "autograd_dense":
+            print(f"(Up to batch size {batch_sizes[-2]})")
+        print(f"Mean = {mean}")
+        print(f"Std = {std}")
+        print("----------------------------------------------")
+
+    print("\n")
+    print("==============================================")
+    print("              SO3.log_map()")
+    for method in ["analytic", "autograd_vmap", "autograd_dense"]:
+        log = []
+        for i in batch_sizes:
+            log.append([])
+
+        itrs = 100
+
+        for itr in range(itrs):
+            for i in range(len(batch_sizes)):
+                if method == "autograd_dense" and batch_sizes[i] > 100:
+                    log[i].append(-1)
+                else:
+                    log[i].append(
+                        test_theseus_log_map_jacobian(batch_sizes[i], method=method)
+                    )
+
+        for i in log:
+            i.sort()
+
+        np_log = np.array(log, dtype=np.float64)
+        # remove top 20% and last 20%, to filt noise
+        np_log = np_log[:, int(itrs * 0.2) : int(itrs * 0.8)]
+
+        mean = np.mean(np_log, axis=1)
+        std = np.std(np_log, axis=1)
+
+        print("----------------------------------------------")
+        jac_type = "Dense" if "dense" in method else "Sparse"
+        print(f"{jac_type} jacobian calculation method: {method}")
+        if method == "autograd_dense":
+            print(f"(Up to batch size {batch_sizes[-2]})")
+        print(f"Mean = {mean}")
+        print(f"Std = {std}")
+        print("----------------------------------------------")
+
+    print("\n")
+    print("==============================================")
+    print("              SO3.compose()")
+    for method in ["analytic", "autograd_vmap", "autograd_dense"]:
+        log = []
+        for i in batch_sizes:
+            log.append([])
+
+        itrs = 100
+
+        for itr in range(itrs):
+            for i in range(len(batch_sizes)):
+                if method == "autograd_dense" and batch_sizes[i] > 100:
+                    log[i].append(-1)
+                else:
+                    log[i].append(
+                        test_theseus_compose_jacobian(batch_sizes[i], method=method)
+                    )
 
         for i in log:
             i.sort()

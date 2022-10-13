a75dddb3a9c0860169029e312a0430e9ed0cc2fb
Luis Pineda <4759586+luisenp@users.noreply.github.com>
2022-10-13 09:39:00 -0700

    Added adaptive damping option to LM.

--- theseus/optimizer/nonlinear/levenberg_marquardt.py
+++ theseus/optimizer/nonlinear/levenberg_marquardt.py
@@ -3,23 +3,41 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Any, Dict, Optional, Type
+from typing import Any, Dict, List, Optional, Sequence, Type, Union
 
 import torch
 
 from theseus.core import Objective
 from theseus.optimizer import Linearization
-from theseus.optimizer.linear import DenseSolver, LinearSolver, LUCudaSparseSolver
+from theseus.optimizer.linear import (
+    CholeskyDenseSolver,
+    LinearSolver,
+    LUCudaSparseSolver,
+    LUDenseSolver,
+)
 
 from .nonlinear_least_squares import NonlinearLeastSquares
 
-_LM_ALLOWED_ELLIPS_DAMP_SOLVERS = [DenseSolver, LUCudaSparseSolver]
+_LM_ALLOWED_ELLIPS_DAMP_SOLVERS: List[Type[LinearSolver]] = [
+    CholeskyDenseSolver,
+    LUDenseSolver,
+    LUCudaSparseSolver,
+]
 _EDAMP_SOLVERS_STR = ",".join(c.__name__ for c in _LM_ALLOWED_ELLIPS_DAMP_SOLVERS)
+_LM_ALLOWED_ADAPTIVE_DAMP_SOLVERS: List[Type[LinearSolver]] = [
+    CholeskyDenseSolver,
+    LUDenseSolver,
+]
+_ADAPT_DAMP_SOLVERS_STR = ",".join(
+    c.__name__ for c in _LM_ALLOWED_ADAPTIVE_DAMP_SOLVERS
+)
 
 
-def _check_ellipsoidal_damping_cls(linear_solver: LinearSolver):
+def _check_linear_solver(
+    linear_solver: LinearSolver, allowed_solvers: Sequence[Type[LinearSolver]]
+):
     good = False
-    for lsc in _LM_ALLOWED_ELLIPS_DAMP_SOLVERS:
+    for lsc in allowed_solvers:
         if isinstance(linear_solver, lsc):
             good = True
     return good
@@ -55,35 +73,54 @@ class LevenbergMarquardt(NonlinearLeastSquares):
             step_size=step_size,
             **kwargs,
         )
-        self._allows_ellipsoidal = _check_ellipsoidal_damping_cls(self.linear_solver)
-        self._damping = 0.001
+        self._allows_ellipsoidal = _check_linear_solver(
+            self.linear_solver, _LM_ALLOWED_ELLIPS_DAMP_SOLVERS
+        )
+        self._allows_adaptive = _check_linear_solver(
+            self.linear_solver, _LM_ALLOWED_ADAPTIVE_DAMP_SOLVERS
+        )
+        self._damping: Union[float, torch.Tensor] = 0.001
         self._ellipsoidal_damping = False
+        self._adaptive_damping = False
 
     def reset(
         self,
         damping: float = 1e-3,
         ellipsoidal_damping: bool = False,
+        adaptive_damping: bool = False,
         **kwargs,
     ) -> None:
-        self._damping = damping
         if ellipsoidal_damping and not self._allows_ellipsoidal:
             raise NotImplementedError(
                 f"Ellipsoidal damping is only supported by solvers with type "
                 f"[{_EDAMP_SOLVERS_STR}]."
             )
         self._ellipsoidal_damping = ellipsoidal_damping
+        if adaptive_damping and not self._allows_adaptive:
+            raise NotImplementedError(
+                f"Adaptive damping is only supported by solvers with type "
+                f"[{_ADAPT_DAMP_SOLVERS_STR}]."
+            )
+        if adaptive_damping:
+            self._damping = damping * torch.ones(
+                self.objective.batch_size,
+                device=self.objective.device,
+                dtype=self.objective.dtype,
+            )
+        else:
+            self._damping = damping
 
     def compute_delta(
         self,
         damping_eps: Optional[float] = None,
         **kwargs,
     ) -> torch.Tensor:
-        if damping_eps and not self._allows_ellipsoidal:
+        if damping_eps is not None and not self._allows_ellipsoidal:
             raise NotImplementedError(
                 f"damping eps is only supported by solvers with type "
                 f"[{_EDAMP_SOLVERS_STR}]."
             )
-        damping_eps = damping_eps or 1e-8
+        damping_eps = damping_eps if damping_eps is not None else 1e-8
 
         return self.linear_solver.solve(
             damping=self._damping,

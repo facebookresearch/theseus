34b53f2d13bd56b168e0af787d06600c0f169fab
Luis Pineda <4759586+luisenp@users.noreply.github.com>
2022-10-19 12:08:13 -0700

    Add missing license headers.

--- theseus/extlib/tests/common.py
+++ theseus/extlib/tests/common.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 _BASPACHO_NOT_INSTALLED_MSG = "Baspacho solver not in theseus extension library."
 
 

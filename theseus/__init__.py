9273e8d2ffe741f095965513beb7edf4e5d8bf28
Luis Pineda <4759586+luisenp@users.noreply.github.com>
2022-10-20 11:16:31 -0700

    Bump version to 0.1.2.

--- theseus/__init__.py
+++ theseus/__init__.py
@@ -2,7 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-__version__ = "0.1.2rc1"
+__version__ = "0.1.2"
 
 
 from .core import (  # usort: skip

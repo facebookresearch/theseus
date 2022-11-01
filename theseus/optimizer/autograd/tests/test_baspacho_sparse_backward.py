f3510dd004c8a5188a3c5eb4a34672bada380140
Luis Pineda <4759586+luisenp@users.noreply.github.com>
2022-10-31 21:40:08 -0700

    Refactored Baspasho solver and linearization autograd test init so that it can be reused in other tests.

--- theseus/optimizer/autograd/tests/test_baspacho_sparse_backward.py
+++ theseus/optimizer/autograd/tests/test_baspacho_sparse_backward.py
@@ -15,11 +15,9 @@ from theseus.utils import random_sparse_binary_matrix, split_into_param_sizes
 import theseus as th
 
 
-def check_sparse_backward_step(
-    batch_size, rows_to_cols_ratio, num_cols, param_size_range, fill, dev="cpu"
+def get_linearization_and_solver_for_random_sparse(
+    batch_size, rows_to_cols_ratio, num_cols, param_size_range, fill, dev, rng
 ):
-    torch.manual_seed(hash(str([batch_size, rows_to_cols_ratio, num_cols, fill])))
-
     # this is necessary assumption, so that the hessian can be full rank. actually we
     # add some damping to At*A's diagonal, so not really necessary
     assert rows_to_cols_ratio >= 1.0
@@ -36,29 +34,48 @@ def check_sparse_backward_step(
     linearization = solver.linearization
 
     A_skel = random_sparse_binary_matrix(
-        num_rows, num_cols, fill, min_entries_per_col=1
+        num_rows, num_cols, fill, min_entries_per_col=1, rng=rng
     )
     void_objective._batch_size = batch_size
     linearization.num_rows = num_rows
     linearization.num_cols = num_cols
     linearization.A_col_ind = A_skel.indices
     linearization.A_row_ptr = A_skel.indptr
-    linearization.A_val = torch.rand((batch_size, A_skel.nnz), dtype=torch.double).to(
-        dev
+    linearization.A_val = torch.rand(
+        (batch_size, A_skel.nnz), dtype=torch.double, device=dev, generator=rng
     )
-    linearization.b = torch.randn((batch_size, num_rows), dtype=torch.double).to(dev)
+    linearization.b = torch.randn(
+        (batch_size, num_rows), dtype=torch.double, device=dev, generator=rng
+    )
 
     # also need: var dims and var_start_cols (because baspacho is blockwise)
-    linearization.var_dims = split_into_param_sizes(num_cols, *param_size_range)
+    linearization.var_dims = split_into_param_sizes(
+        num_cols, *param_size_range, rng=rng
+    )
     linearization.var_start_cols = np.cumsum([0, *linearization.var_dims[:-1]])
 
+    return linearization, solver
+
+
+def check_sparse_backward_step(
+    batch_size, rows_to_cols_ratio, num_cols, param_size_range, fill, dev="cpu"
+):
+    rng = torch.Generator(device=dev)
+    rng.manual_seed(hash(str([batch_size, rows_to_cols_ratio, num_cols, fill])))
+
+    linearization, solver = get_linearization_and_solver_for_random_sparse(
+        batch_size, rows_to_cols_ratio, num_cols, param_size_range, fill, dev, rng
+    )
     linearization.A_val.requires_grad = True
     linearization.b.requires_grad = True
     # Only need this line for the test since the objective is a mock
     solver.reset(dev=dev)
+    alpha = torch.rand(batch_size, device=dev, dtype=torch.double, generator=rng)
+    if torch.rand(1, device=rng.device, generator=rng).item() < 0.5:
+        alpha = torch.zeros_like(alpha)  # also test non-ellipsoidal
     damping_alpha_beta = (
-        0.023 * torch.rand_like(linearization.A_val[:, 0]),
-        0.157 * torch.rand_like(linearization.A_val[:, 0]),
+        alpha,
+        torch.rand(batch_size, device=dev, dtype=torch.double, generator=rng),
     )
     inputs = (
         linearization.A_val,

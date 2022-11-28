Fixed numerical issues in Dogleg.

theseus/optimizer/nonlinear/dogleg.py
@@ -16,6 +16,8 @@ from .trust_region import TrustRegion
 # See Nocedal and Wright, Numerical Optimization, pp. 73-77
 # https://www.csie.ntu.edu.tw/~r97002/temp/num_optimization.pdf
 class Dogleg(TrustRegion):
+    EPS = 1e-7
+
     def __init__(
         self,
         objective: Objective,
@@ -56,7 +58,8 @@ class Dogleg(TrustRegion):
         good_gn_idx = delta_gn_norm_2 < trust_region_2
         # All Gauss-Newton step are within trust-region, can return
         if good_gn_idx.all():
-            return delta_gn, good_gn_idx
+            # Return a False mask since no indices are at the boundary
+            return delta_gn, torch.zeros_like(good_gn_idx).bool()
 
         # If reach here, some steps are outside trust-region,
         # need to compute dogleg step
@@ -73,23 +76,26 @@ class Dogleg(TrustRegion):
             Adelta_sd = linearization.A.bmm(neg_grad.unsqueeze(2)).squeeze(2)
             Adelta_sd_norm_2 = TrustRegion._detached_squared_norm(Adelta_sd)
             grad_norm_2 = TrustRegion._detached_squared_norm(neg_grad)
-            t = grad_norm_2 / Adelta_sd_norm_2
+            t = grad_norm_2 / (Adelta_sd_norm_2 + Dogleg.EPS)
             delta_sd = neg_grad * t
             delta_sd_norm_2 = TrustRegion._detached_squared_norm(delta_sd)
             not_near_zero_sd_idx = delta_sd_norm_2 > 1e-6
-            good_sd_idx = delta_sd_norm_2 <= trust_region_2
+            sd_within_region_idx = delta_sd_norm_2 <= trust_region_2
 
-        delta_dogleg = delta_sd
-        # If the steepest descent direction is too close to zero, just use
-        # the Gauss-Newton direction truncated at the trust region to avoid
-        # numerical errors.
-        delta_dogleg = torch.where(
-            not_near_zero_sd_idx,
-            delta_dogleg,
-            delta_gn / delta_gn_norm_2.sqrt() * self._trust_region,
-        )
-        good_sd_idx &= not_near_zero_sd_idx
-        if good_sd_idx.any():
+        # Firsst make sure that any steps beyond the trust region, are truncated
+        if not sd_within_region_idx.all():
+            delta_dogleg = torch.where(
+                sd_within_region_idx,
+                delta_sd,
+                delta_sd * self._trust_region / (delta_sd_norm_2 + Dogleg.EPS).sqrt(),
+            )
+        else:
+            delta_dogleg = delta_sd
+
+        # Now mask near zero indices so the next computation doesn't happen for them
+        sd_within_region_idx &= not_near_zero_sd_idx
+
+        if sd_within_region_idx.any():
             # In this case, some steepest descent steps are within region
             # so need to extend towards boundary with Gauss-Newton step
             # Need to solve a quadratic || sd + tau * (gn - sd)|| == tr**2
@@ -100,18 +106,28 @@ class Dogleg(TrustRegion):
                 a = TrustRegion._detached_squared_norm(diff)
                 b = (2 * delta_sd * diff).sum(dim=1, keepdim=True)
                 c = delta_sd_norm_2 - trust_region_2
-                disc = ((b**2) - 4 * a * c).clamp(0.0)
-                tau = (-b + disc.sqrt()) / (2 * a)
-            delta_dogleg = torch.where(good_sd_idx, delta_sd + tau * diff, delta_sd)
-
-        if not good_sd_idx.all():
-            # For any steps beyond the trust region, truncate to trust region
+                disc = ((b**2) - 4 * a * c) + Dogleg.EPS
+                tau = (-b + disc.sqrt()) / (2 * a + Dogleg.EPS)
+                tau[~sd_within_region_idx] = 0.0  # avoid nans in backward pass
             delta_dogleg = torch.where(
-                good_sd_idx,
+                sd_within_region_idx,
+                delta_sd + tau * diff,
                 delta_dogleg,
-                delta_sd * trust_region_2 / delta_sd_norm_2.sqrt(),
+            )
+
+        # Finally, when the steepest descent direction is too close to zero, just use
+        # the Gauss-Newton direction truncated at the trust region, to avoid
+        # numerical errors.
+        if not not_near_zero_sd_idx.all():
+            delta_dogleg = torch.where(
+                not_near_zero_sd_idx,
+                delta_dogleg,
+                delta_gn / (delta_gn_norm_2 + Dogleg.EPS).sqrt() * self._trust_region,
             )
 
         # Finally, use the Gauss-Newton step if it was within the boundary
         delta_dogleg = torch.where(good_gn_idx, delta_gn, delta_dogleg)
-        return delta_dogleg, good_gn_idx
+        # The only steps that are within the trust region are those were
+        # Gauss-Newton was "good". Every other step size will have
+        # norm exactly equal to the trust region
+        return delta_dogleg, ~good_gn_idx

theseus/optimizer/nonlinear/trust_region.py
@@ -33,8 +33,8 @@ from .nonlinear_least_squares import NonlinearLeastSquares
 # then the trust region is reduced by `shrink_ratio`.
 # The trust region is initialized to `trust_region_init`.
 class TrustRegion(NonlinearLeastSquares, abc.ABC):
-    _MIN_TRUST_REGION = 1.0e-7
-    _MAX_TRUST_REGION = 1.0e7
+    _MIN_TRUST_REGION = 1.0e-5
+    _MAX_TRUST_REGION = 1.0e5
 
     def __init__(
         self,
@@ -69,11 +69,11 @@ class TrustRegion(NonlinearLeastSquares, abc.ABC):
             # I expect this method to work with all our current solvers
             raise NotImplementedError
         self._trust_region: torch.Tensor = None
-        self._trusted_step_idx: torch.Tensor = None
+        self._at_trust_boundary_idx: torch.Tensor = None
 
     def reset(
         self,
-        trust_region_init: float = 1.0,
+        trust_region_init: float = 0.5,
         **kwargs,
     ) -> None:
         self._trust_region = trust_region_init * torch.ones(
@@ -82,10 +82,10 @@ class TrustRegion(NonlinearLeastSquares, abc.ABC):
             device=self.objective.device,
             dtype=self.objective.dtype,
         )
-        self._trusted_step_idx = None
+        self._at_trust_boundary_idx = None
 
     # Return the computed delta and, optionally, the indices that with the
-    # steps that are within the trust region
+    # steps that are exactly at the boundary of the trust region
     @abc.abstractmethod
     def _compute_delta_impl(self) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
         pass
@@ -97,7 +97,16 @@ class TrustRegion(NonlinearLeastSquares, abc.ABC):
         if self._trusted_step_idx is None:
             self._trusted_step_idx = (
                 TrustRegion._detached_squared_norm(delta) <= self._trust_region**2
-                <= self._trust_region**2
+            )
+
+        # Storing the indices of delta exactly at the trust boundary.
+        # If the step is at the boundary and the predicted error reduction agrees
+        # with real reduction, we can increase the trust region
+        # in self.complete_step()
+        delta, self._at_trust_boundary_idx = self._compute_delta_impl()
+        if self._at_trust_boundary_idx is None:
+            self._at_trust_boundary_idx = (
+                TrustRegion._detached_squared_norm(delta) <= self._trust_region**2
             )
         return delta
 
@@ -138,7 +147,7 @@ class TrustRegion(NonlinearLeastSquares, abc.ABC):
         delta: torch.Tensor,
         new_err: torch.Tensor,
         previous_err: torch.Tensor,
-        accept_threshold: float = 0.25,
+        accept_threshold: float = 0.0,
         shrink_threshold: float = 0.25,
         expand_threshold: float = 0.75,
         shrink_ratio: float = 0.25,
@@ -147,12 +156,12 @@ class TrustRegion(NonlinearLeastSquares, abc.ABC):
     ) -> Optional[torch.Tensor]:
         good_params = (0.0 < shrink_ratio <= 1.0) and (expand_ratio >= 1.0)
         good_params &= (shrink_threshold < expand_threshold) and (
-            accept_threshold < expand_threshold
+            accept_threshold < shrink_threshold
         )
         if not good_params:
             raise ValueError(
                 "Invalid parameters for TrustRegionMethod. "
-                "Values must satisfy accept_threshold/shrink_threshold < expand_threshold, "
+                "Values must satisfy <accept/shrink>_threshold < expand_threshold, "
                 "shrink_ratio in (0, 1], and expand_ratio > 1.0."
             )
         pred_err = self._predicted_error(previous_err, delta)
@@ -161,7 +170,7 @@ class TrustRegion(NonlinearLeastSquares, abc.ABC):
         self._trust_region = torch.where(
             shrink_idx, self._trust_region * shrink_ratio, self._trust_region
         )
-        expand_idx = (rho > expand_threshold) & ~self._trusted_step_idx
+        expand_idx = (rho > expand_threshold) & self._at_trust_boundary_idx
         self._trust_region = torch.where(
             expand_idx, self._trust_region * expand_ratio, self._trust_region
         )
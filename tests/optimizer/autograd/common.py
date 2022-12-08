914f20410f913c14edeade9ac9e49313a17ea22b
Luis Pineda <4759586+luisenp@users.noreply.github.com>
2022-12-08 08:58:08 -0800

    Remove print statement and add explanatory comment.

--- tests/optimizer/autograd/common.py
+++ tests/optimizer/autograd/common.py
@@ -13,9 +13,18 @@ def check_grad(solve_func, inputs, eps, atol, rtol):
     A_float = A_val.float()
     b_float = b.float()
     inputs2 = (A_float, b_float) + inputs[2:]
-    print(type(inputs2), len(inputs2))
     out_float = solve_func(*inputs2).sum()
     gA_float, gb_float = grad(out_float, (A_float, b_float))
 
+    # This mostly checks that backward() is not accumulating
+    # additional floating point errors on top of those expected by
+    # converting the input from double to float. Naive float casting
+    # in the backward python ops can result in differences in order of magnitude
+    # even for well-conditioned systems. These checks cover that case.
+    #
+    # Do note that, in general, it's possible to construct very
+    # ill-conditioned systems where the initial loss or precision is enough
+    # to get large gradient errors no matter what we do. This checks
+    # are not meant to capture such scenarios.
     torch.testing.assert_close(gA, gA_float.double(), rtol=rtol, atol=atol)
     torch.testing.assert_close(gb, gb_float.double(), rtol=rtol, atol=atol)

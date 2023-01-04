b2ea6f37057e3d00f1b82865f6f3d1c7e9c65c28
Taosha Fan <fantaosha@gmail.com>
2023-01-04 12:06:39 -0800

    add jacobian tests for forward kinematics

--- tests/embodied/robot/test_forward_kinematics.py
+++ tests/embodied/robot/test_forward_kinematics.py
@@ -6,6 +6,7 @@
 import pytest
 import os
 import torch
+from theseus.geometry.functional import se3
 from theseus.geometry.functional.constants import TEST_EPS
 from theseus.embodied.robot.forward_kinematics import Robot
 from theseus.embodied.robot.forward_kinematics import (
@@ -20,7 +21,7 @@ urdf_path = os.path.join(os.path.dirname(__file__), URDF_REL_PATH)
 
 @pytest.mark.parametrize("batch_size", [1, 20, 40])
 @pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
-def test_forward_kinematics(batch_size: int, dtype: torch.dtype):
+def test_backward(batch_size: int, dtype: torch.dtype):
     robot = Robot.from_urdf_file(urdf_path, dtype)
     selected_links = ["panda_link2", "panda_link5", "panda_virtual_ee_link"]
     _, fkin_impl, _, _, _ = ForwardKinematicsFactory(robot, selected_links)
@@ -35,3 +36,29 @@ def test_forward_kinematics(batch_size: int, dtype: torch.dtype):
 
     for jac, jac_impl in zip(jacs, jacs_impl):
         assert torch.allclose(jac_impl, jac, atol=TEST_EPS)
+
+
+@pytest.mark.parametrize("batch_size", [1, 20, 40])
+@pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
+def test_jacobian(batch_size: int, dtype: torch.dtype):
+    robot = Robot.from_urdf_file(urdf_path, dtype)
+    selected_links = ["panda_link2", "panda_link5", "panda_virtual_ee_link"]
+    fkin, jfkin = get_forward_kinematics(robot, selected_links)
+
+    rng = torch.Generator()
+    rng.manual_seed(0)
+    angles = torch.rand(batch_size, robot.dof, generator=rng, dtype=dtype)
+
+    jacs_actual, poses = jfkin(angles)
+
+    sels = range(batch_size)
+    jacs_dense = torch.autograd.functional.jacobian(fkin, angles)
+    jacs_expected = []
+    for pose, jac_dense in zip(poses, jacs_dense):
+        jac_dense = jac_dense[sels, :, :, sels].transpose(-1, 1).transpose(-1, -2)
+        jac_expected = se3.left_project(pose, jac_dense).transpose(-1, -2)
+        jac_expected[:, 3:] *= 0.5
+        jacs_expected.append(jac_expected)
+
+    for jac_actual, jac_expected in zip(jacs_actual, jacs_expected):
+        assert torch.allclose(jac_actual, jac_expected, atol=1e-6)

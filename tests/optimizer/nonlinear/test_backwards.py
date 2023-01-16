56b928dc61637bcd0760defc8057fc7ffbd0b124
Brandon Amos <brandon.amos.cs@gmail.com>
2023-01-16 14:31:25 -0500

    add quartic test

--- tests/optimizer/nonlinear/test_backwards.py
+++ tests/optimizer/nonlinear/test_backwards.py
@@ -12,38 +12,6 @@ import theseus as th
 torch.manual_seed(0)
 
 
-def generate_data(num_points=10, a=1.0, b=0.5, noise_factor=0.01):
-    data_x = torch.rand((1, num_points))
-    noise = torch.randn((1, num_points)) * noise_factor
-    data_y = a * data_x.square() + b + noise
-    return data_x, data_y
-
-
-num_points = 10
-data_x, data_y = generate_data(num_points)
-
-x = th.Variable(data_x.requires_grad_(), name="x")
-y = th.Variable(data_y.requires_grad_(), name="y")
-a = th.Vector(1, name="a")
-b = th.Vector(1, name="b")
-
-
-def quad_error_fn(optim_vars, aux_vars):
-    a, b = optim_vars
-    x, y = aux_vars
-    est = a.tensor * x.tensor.square() + b.tensor
-    err = y.tensor - est
-    return err
-
-
-def quad_sq_error_fn(optim_vars, aux_vars):
-    a, b = optim_vars
-    x, y = aux_vars
-    est = a.tensor * x.tensor.square() + b.tensor
-    err = (y.tensor - est) ** 2
-    return err
-
-
 def numeric_grad(f, h=1e-4):
     # Approximate the gradient with a central difference.
     def df(x):
@@ -59,113 +27,179 @@ def numeric_grad(f, h=1e-4):
     return df
 
 
-def test_backwards():
-    for error_fn in [quad_error_fn, quad_sq_error_fn]:
-        optim_vars = [a, b]
-        aux_vars = [x, y]
-        cost_function = th.AutoDiffCostFunction(
-            optim_vars,  # type: ignore
-            error_fn,
-            num_points,
-            aux_vars=aux_vars,
-            name="quadratic_cost_fn",
-        )
-        objective = th.Objective()
-        objective.add(cost_function)
-        optimizer = th.GaussNewton(
-            objective,
-            max_iterations=15,
-            step_size=1.0,
-            abs_err_tolerance=1e-15,
-        )
+def test_backwards_quad_fit():
+    def generate_data(num_points=10, a=1.0, b=0.5, noise_factor=0.01):
+        data_x = torch.rand((1, num_points))
+        noise = torch.randn((1, num_points)) * noise_factor
+        data_y = a * data_x.square() + b + noise
+        return data_x, data_y
 
-        theseus_inputs = {
-            "a": 2 * torch.ones((1, 1)).requires_grad_(),
-            "b": torch.ones((1, 1)).requires_grad_(),
-            "x": data_x,
-            "y": data_y,
-        }
-        theseus_optim = th.TheseusLayer(optimizer)
+    num_points = 10
+    data_x, data_y = generate_data(num_points)
 
-        # First we use torch.autograd.functional to numerically compute the gradient
-        # the optimal a w.r.t. the x part of the data
-        with torch.no_grad():
+    x = th.Variable(data_x.requires_grad_(), name="x")
+    y = th.Variable(data_y.requires_grad_(), name="y")
+    a = th.Vector(1, name="a")
+    b = th.Vector(1, name="b")
 
-            def fit_x(data_x_np):
-                theseus_inputs["x"] = (
-                    torch.from_numpy(data_x_np)
-                    .float()
-                    .clone()
-                    .requires_grad_()
-                    .unsqueeze(0)
-                )
-                updated_inputs, _ = theseus_optim.forward(
-                    theseus_inputs,
-                    optimizer_kwargs={"track_best_solution": True, "verbose": False},
-                )
-                return updated_inputs["a"].item()
+    def error_fn(optim_vars, aux_vars):
+        a, b = optim_vars
+        x, y = aux_vars
+        est = a.tensor * x.tensor.square() + b.tensor
+        err = y.tensor - est
+        return err
 
-            data_x_np = data_x.detach().clone().numpy().squeeze()
-            dfit_x = numeric_grad(fit_x, h=1e-4)
-            da_dx_numeric = torch.from_numpy(dfit_x(data_x_np)).float()
+    optim_vars = [a, b]
+    aux_vars = [x, y]
+    cost_function = th.AutoDiffCostFunction(
+        optim_vars,  # type: ignore
+        error_fn,
+        num_points,
+        aux_vars=aux_vars,
+        name="quadratic_cost_fn",
+    )
+    objective = th.Objective()
+    objective.add(cost_function)
+    optimizer = th.GaussNewton(
+        objective,
+        max_iterations=15,
+        step_size=1.0,
+    )
 
-        theseus_inputs["x"] = data_x
-        updated_inputs, _ = theseus_optim.forward(
-            theseus_inputs,
-            optimizer_kwargs={
-                "track_best_solution": True,
-                "verbose": False,
-                "backward_mode": "unroll",
-            },
-        )
-        da_dx_unroll = torch.autograd.grad(
-            updated_inputs["a"], data_x, retain_graph=True
-        )[0].squeeze()
-        tol = 1e-3 if error_fn == quad_error_fn else 1e-1
-        torch.testing.assert_close(da_dx_numeric, da_dx_unroll, atol=tol, rtol=tol)
+    theseus_inputs = {
+        "a": 2 * torch.ones((1, 1)).requires_grad_(),
+        "b": torch.ones((1, 1)).requires_grad_(),
+        "x": data_x,
+        "y": data_y,
+    }
+    theseus_optim = th.TheseusLayer(optimizer)
 
-        updated_inputs, _ = theseus_optim.forward(
-            theseus_inputs,
-            optimizer_kwargs={
-                "track_best_solution": True,
-                "verbose": False,
-                "backward_mode": th.BackwardMode.IMPLICIT,
-            },
-        )
-        da_dx_implicit = torch.autograd.grad(
-            updated_inputs["a"], data_x, retain_graph=True
-        )[0].squeeze()
-        tol = 1e-3 if error_fn == quad_error_fn else 0.3
-        torch.testing.assert_close(da_dx_numeric, da_dx_implicit, atol=tol, rtol=tol)
+    # First we use torch.autograd.functional to numerically compute the gradient
+    # the optimal a w.r.t. the x part of the data
+    with torch.no_grad():
 
-        updated_inputs, _ = theseus_optim.forward(
-            theseus_inputs,
-            optimizer_kwargs={
-                "track_best_solution": True,
-                "verbose": False,
-                "backward_mode": "TRUNCATED",
-                "backward_num_iterations": 5,
-            },
-        )
-        da_dx_truncated = torch.autograd.grad(
-            updated_inputs["a"], data_x, retain_graph=True
-        )[0].squeeze()
-        tol = 1e-3 if error_fn == quad_error_fn else 1e-1
-        torch.testing.assert_close(da_dx_numeric, da_dx_truncated, atol=tol, rtol=tol)
-
-        if error_fn == quad_error_fn:
+        def fit_x(data_x_np):
+            theseus_inputs["x"] = (
+                torch.from_numpy(data_x_np)
+                .float()
+                .clone()
+                .requires_grad_()
+                .unsqueeze(0)
+            )
             updated_inputs, _ = theseus_optim.forward(
                 theseus_inputs,
-                optimizer_kwargs={
-                    "track_best_solution": True,
-                    "verbose": False,
-                    "backward_mode": th.BackwardMode.DLM,
-                    "dlm_epsilon": 0.0001,
-                },
+                optimizer_kwargs={"track_best_solution": True, "verbose": False},
             )
-            da_dx_truncated = torch.autograd.grad(
-                updated_inputs["a"], data_x, retain_graph=True
-            )[0].squeeze()
-            torch.testing.assert_close(
-                da_dx_numeric, da_dx_truncated, atol=1e-1, rtol=1e-1
-            )
+            return updated_inputs["a"].item()
+
+        data_x_np = data_x.detach().clone().numpy().squeeze()
+        dfit_x = numeric_grad(fit_x, h=1e-4)
+        da_dx_numeric = torch.from_numpy(dfit_x(data_x_np)).float()
+
+    theseus_inputs["x"] = data_x
+    updated_inputs, _ = theseus_optim.forward(
+        theseus_inputs,
+        optimizer_kwargs={
+            "track_best_solution": True,
+            "verbose": False,
+            "backward_mode": "unroll",
+        },
+    )
+    da_dx_unroll = torch.autograd.grad(updated_inputs["a"], data_x, retain_graph=True)[
+        0
+    ].squeeze()
+    torch.testing.assert_close(da_dx_numeric, da_dx_unroll, atol=1e-3, rtol=1e-3)
+
+    updated_inputs, _ = theseus_optim.forward(
+        theseus_inputs,
+        optimizer_kwargs={
+            "track_best_solution": True,
+            "verbose": False,
+            "backward_mode": th.BackwardMode.IMPLICIT,
+        },
+    )
+    da_dx_implicit = torch.autograd.grad(
+        updated_inputs["a"], data_x, retain_graph=True
+    )[0].squeeze()
+    torch.testing.assert_close(da_dx_numeric, da_dx_implicit, atol=1e-3, rtol=1e-3)
+
+    updated_inputs, _ = theseus_optim.forward(
+        theseus_inputs,
+        optimizer_kwargs={
+            "track_best_solution": True,
+            "verbose": False,
+            "backward_mode": "TRUNCATED",
+            "backward_num_iterations": 5,
+        },
+    )
+    da_dx_truncated = torch.autograd.grad(
+        updated_inputs["a"], data_x, retain_graph=True
+    )[0].squeeze()
+    torch.testing.assert_close(da_dx_numeric, da_dx_truncated, atol=1e-3, rtol=1e-3)
+
+    updated_inputs, _ = theseus_optim.forward(
+        theseus_inputs,
+        optimizer_kwargs={
+            "track_best_solution": True,
+            "verbose": False,
+            "backward_mode": th.BackwardMode.DLM,
+            "dlm_epsilon": 0.0001,
+        },
+    )
+    da_dx_truncated = torch.autograd.grad(
+        updated_inputs["a"], data_x, retain_graph=True
+    )[0].squeeze()
+    torch.testing.assert_close(da_dx_numeric, da_dx_truncated, atol=1e-1, rtol=1e-1)
+
+
+def test_backwards_quartic():
+    def error_fn(optim_vars, aux_vars):
+        (a,) = optim_vars
+        (x,) = aux_vars
+        err = (a.tensor - x.tensor) ** 2
+        return err
+
+    a = th.Vector(1, name="a")
+    x_th = torch.zeros([1, 1]).requires_grad_()
+    x = th.Variable(x_th, name="x")
+    optim_vars = [a]
+    aux_vars = [x]
+    cost_function = th.AutoDiffCostFunction(
+        optim_vars,
+        error_fn,
+        1,
+        aux_vars=aux_vars,
+        name="cost_fn",
+    )
+    objective = th.Objective()
+    objective.add(cost_function)
+    optimizer = th.GaussNewton(
+        objective,
+        max_iterations=15,
+        step_size=1.0,
+    )
+
+    theseus_inputs = {
+        "a": torch.ones([1, 1]).requires_grad_(),
+        "x": x_th,
+    }
+    theseus_optim = th.TheseusLayer(optimizer)
+
+    updated_inputs, info = theseus_optim.forward(
+        theseus_inputs,
+        optimizer_kwargs={
+            "track_best_solution": True,
+            "verbose": False,
+            "backward_mode": "implicit",
+        },
+    )
+
+    da_dx = torch.autograd.grad(updated_inputs["a"], x_th, retain_graph=True)[
+        0
+    ].squeeze()
+    print("\n--- backward_mode=implicit")
+    print(da_dx.numpy())
+
+    # Equality should hold exactly even in floating point
+    # because of how the derivatives cancel
+    assert da_dx.item() == 1.5

a7da01117f3d75a1d2c0ff2c5a15c63bef8262a5
Brandon Amos <brandon.amos.cs@gmail.com>
2023-02-02 21:52:12 -0500

    car: tweak hypers, add control plot

--- scratch/car.py
+++ scratch/car.py
@@ -7,10 +7,12 @@
 import math
 import numpy as np
 import torch
+import copy
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+plt.style.use('bmh')
 
 import theseus as th
 import sys
@@ -21,20 +23,22 @@ sys.excepthook = ultratb.FormattedTB(mode="Plain", color_scheme="Neutral", call_
 torch.manual_seed(0)
 torch.set_default_dtype(torch.float64)
 
-N = 30
-L = 0.05
+N = 20
+L = 0.1
 x_init = torch.zeros(3)
 # x_final = torch.tensor([0, 0.5, 0])
 # x_final = torch.tensor([0, 1., 0])
 # x_final = torch.tensor([0.5, 0.3, 0])
 # x_final = torch.tensor([0, 1., 0.5*np.pi])
-# x_final = torch.tensor([0.5, 0.5, np.pi/2.])
-x_final = torch.tensor([0.5, 0.5, 0.])
-h = 1.
-gamma = 10.
+# x_final = torch.tensor([0.5, 0.5, 0.])
+x_final = torch.tensor([0.5, 0.3, np.pi/2.])
+h = 0.05
+
+squared_cost_weight = 1e-2
+slew_cost_weight = 1e-1
 
 
-def plot(xs, us):
+def plot_car(xs, us):
     xs = [x_init] + xs + [x_final]
     us = us + [torch.zeros(2)]
 
@@ -46,8 +50,8 @@ def plot(xs, us):
         p = x[:2]
         theta = x[2]
 
-        width = 0.02
-        alpha = 0.7
+        width = 0.5*L
+        alpha = 0.5
         rect = patches.Rectangle(
             (0, -0.5*width), L, width, linewidth=1,
             edgecolor='black', facecolor='#DCDCDC', alpha=alpha)
@@ -74,17 +78,28 @@ def plot(xs, us):
 
     ax.axis('equal')
     fig.savefig('t.png')
+    plt.close(fig)
+
+def plot_actions(us):
+    us = torch.stack(us).squeeze()
+    fig, ax = plt.subplots(figsize=(6,4))
+    ax.plot(us[:,0], label='speed')
+    ax.plot(us[:,1], label='steering angle')
+    fig.legend(ncol=2, loc='upper center')
+    fig.savefig('actions.png', transparent=True)
+    plt.close(fig)
 
 def g(us, xs):
+    assert len(us) == N
     constraints = [dx(x_init, us[0][0]) - xs[0]]
     for idx in range(N - 2):
-        constraints.append(dx(xs[idx][0], us[idx][0]) - xs[idx + 1])
+        constraints.append(dx(xs[idx][0], us[idx+1][0]) - xs[idx + 1])
     constraints.append((dx(xs[-1][0], us[-1][0]) - x_final).unsqueeze(0))
-    return torch.cat(constraints).ravel()
+    constraints = torch.cat(constraints).ravel()
+    return constraints
 
 
 def dx(x, u):
-    # new_x = x.clone()
     new_x = [
         x[0] + h * u[0] * torch.cos(x[2]),
         x[1] + h * u[0] * torch.sin(x[2]),
@@ -98,25 +113,23 @@ def aug_lagrang(mu, z, state_dict):
         us, xs = optim_vars[:N], optim_vars[N:]
         us = [u.tensor for u in us]
         xs = [x.tensor for x in xs]
-        cost_list = us
+        assert len(us) == N
+        assert len(xs) == N - 1
+        cost_list = copy.copy([u*squared_cost_weight for u in us])
         for idx in range(len(us) - 1):
-            cost_list.append(math.sqrt(gamma) * (us[idx + 1] - us[idx]))
+            cost_list.append(math.sqrt(slew_cost_weight) * (us[idx + 1] - us[idx]))
 
         mu, z = aux_vars
         augment = torch.sqrt(mu.tensor) * g(us, xs) + z.tensor / (
             2.0 * torch.sqrt(mu.tensor)
         )
-        # cost = torch.cat([x.tensor, augment], axis=-1)
         cost = torch.cat(cost_list + [augment], axis=-1)
-        # print(cost.shape)
         return cost
 
     n_dim = N * 2
     n_constraints = z.shape[-1]
     u = [th.Vector(2, name="u" + str(idx + 1)) for idx in range(N)]
     x = [th.Vector(3, name="x" + str(idx + 1)) for idx in range(1, N)]
-    # mu = th.Variable(torch.ones(1, 1) * 1000, name="mu")
-    # z = th.Variable(torch.ones(1, 1) * 1, name="z")
     optim_vars = u + x
     aux_vars = [mu, z]
     cost_function = th.AutoDiffCostFunction(
@@ -128,10 +141,10 @@ def aug_lagrang(mu, z, state_dict):
     )
     objective = th.Objective()
     objective.add(cost_function)
-    optimizer = th.GaussNewton(
+    optimizer = th.LevenbergMarquardt(
         objective,
-        max_iterations=10000,
-        step_size=0.1,
+        max_iterations=100,
+        step_size=1.,
     )
 
     theseus_optim = th.TheseusLayer(optimizer)
@@ -153,47 +166,38 @@ def aug_lagrang(mu, z, state_dict):
 
 
 mu = th.Variable(torch.ones(1, 1), name="mu")
+# mu = th.Variable(torch.ones(1, 1), name="mu")
 z = th.Variable(torch.zeros(1, N * 3), name="z")
 
 state_dict = {}
 
-def project_states(state_dict):
-    xt = x_init
-    for t in range(1, N+1):
-        # print(t)
-        ut = state_dict.get(f'u{t}')
-        # state_dict[f'u{t}'] = ut.unsqueeze(0)
-        # print('h')
-        state_dict[f'x{t+1}'] = dx(xt, ut.squeeze()).unsqueeze(0)
-    # import ipdb; ipdb.set_trace()
-
 prev_g_norm = 0
 
-# increase_factor = 2.
+increase_factor = 2.
 
-# print(x)
+state_dict = {}
+
 for i in range(50):
     print(f'=== iter {i}')
-    # if i > 0:
-    #     project_states(state_dict)
     state_dict = aug_lagrang(mu, z, state_dict=state_dict)
     us = [state_dict["u" + str(idx + 1)] for idx in range(N)]
     xs = [state_dict["x" + str(idx + 1)] for idx in range(1, N)]
-    # us = [u.tensor for u in us]
-    # xs = [x.tensor for x in xs]
 
     g_x = g(us, xs)
     z.tensor = z.tensor + 2 * mu.tensor * g_x
     g_norm = g_x.norm()
     if g_norm > 0.25 * prev_g_norm:
-        mu.tensor = 2 * mu.tensor
+        mu.tensor = increase_factor * mu.tensor
     prev_g_norm = g_norm
 
     print(xs)
     print(us)
     print(mu)
     print(z)
+    print(g_x)
 
-    plot(xs, us)
-    # import ipdb; ipdb.set_trace()
+    plot_car(xs, us)
+    plot_actions(us)
+
+# import ipdb; ipdb.set_trace()
 

074c2f9686f6f6f5c6d1cf5dc0a6687a5b8dcf77
Taosha Fan <6612911+fantaosha@users.noreply.github.com>
2023-03-01 16:25:01 -0500

    Add robot model (#417)
    * add link
    
    * add id to joint
    
    * add id to link
    
    * add robot model
    
    * add name to robot
    
    * backup
    
    * refactor joint
    
    * refactor link
    
    * add robot model
    
    * robot model works
    
    * a minior refactor of joint
    
    * add ancestor to links
    
    * update dof computation
    
    * add forward kinematics function
    
    * Add differentiable forward kinematics (#420)
    
    * fixed a bug in FixedJoint
    
    * add angle_ids
    
    * add jacobians to FK
    
    * forward kinematics works
    
    * rename FK
    
    * add tests for differentiable kinematics
    
    * rename tests
    
    * simplify the computation of origin
    
    * add jacobian tests for forward kinematics
    
    * fixed a bug for vectorize=True in SE3
    
    * improve the efficiency of forward kinematics
    
    * add tests for backward propagation
    
    * update lie group load paths
    
    * Add device to robot model (#422)
    
    * add device to robot model
    
    * Refactor Joint.dof as a property (#423)
    
    * refactor joint dof
    
    * add virtual end-effector for tests
    
    * seems to work after name changes
    
    * rename parent->parent_link and child->child_link
    
    * rename angle_ids -> ancestor_active_joint_ids
    
    * refactor tests with torch.testing.assert_close
    
    * small refactoring of the code
    
    * add Robot.get_links()
    
    * remove some setters for link and joint
    
    * made some functions private for encapsulation
    
    * consolidate if block and classmethod->staticmethod
    
    * refactor set_robot_from_urdf_file
    
    * rename ancestor_active_joint_ids -> ancestor_non_fixed_joint_ids
    
    * id=-1 -> id=None
    
    * refactor the code for simplicity
    
    * move link into joint

---
+++ theseus/embodied/robot/forward_kinematics.py
@@ -0,0 +1,139 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+
+from typing import List, Optional
+
+from theseus.labs.lie_functional import se3
+from .robot import Robot, Joint, Link
+
+# TODO: Add support for joints with DOF>1
+
+
+def ForwardKinematicsFactory(robot: Robot, link_names: Optional[List[str]] = None):
+    links: List[Link] = robot.get_links(link_names)
+    link_ids: List[int] = [link.id for link in links]
+
+    ancestor_links: List[Link] = []
+    joint_ids: List[int] = []
+    for link in links:
+        ancestor_links += [anc for anc in link.ancestor_links]
+        joint_ids += link.ancestor_non_fixed_joint_ids
+    pose_ids = sorted(list(set([anc.id for anc in ancestor_links] + link_ids)))
+    joint_ids = sorted(list(set(joint_ids)))
+
+    def _forward_kinematics_helper(angles: torch.Tensor):
+        if angles.ndim != 2 or angles.shape[1] != robot.dof:
+            raise ValueError(
+                f"Joint angles for {robot.name} should be {robot.dof}-D vectors"
+            )
+
+        poses: List[Optional[torch.Tensor]] = [None] * robot.num_links
+        poses[0] = angles.new_zeros(angles.shape[0], 3, 4)
+        poses[0][:, 0, 0] = 1
+        poses[0][:, 1, 1] = 1
+        poses[0][:, 2, 2] = 1
+
+        for id in pose_ids[1:]:
+            curr: Link = robot.links[id]
+            joint: Joint = robot.links[id].parent_joint
+            prev: Link = joint.parent_link
+            relative_pose = (
+                joint.relative_pose(angles[:, joint.id])
+                if joint.id < robot.dof
+                else joint.relative_pose()
+            )
+            poses[curr.id] = se3.compose(poses[prev.id], relative_pose)
+
+        return poses
+
+    def _forward_kinematics_impl(angles: torch.Tensor):
+        poses = _forward_kinematics_helper(angles)
+        return tuple(poses[id] for id in link_ids)
+
+    def _jforward_kinematics_helper(
+        poses: List[Optional[torch.Tensor]],
+    ) -> torch.Tensor:
+        jposes = poses[0].new_zeros(poses[0].shape[0], 6, robot.dof)
+
+        for id in pose_ids[1:]:
+            link: Link = robot.links[id]
+            joint: Joint = link.parent_joint
+            if joint.id >= robot.dof:
+                break
+            jposes[:, :, joint.id : joint.id + 1] = (
+                se3.adjoint(poses[link.id]) @ joint.axis
+            )
+
+        return jposes
+
+    def _jforward_kinematics_impl(angles: torch.Tensor):
+        poses = _forward_kinematics_helper(angles)
+        jposes = _jforward_kinematics_helper(poses)
+
+        rets = tuple(poses[id] for id in link_ids)
+        jacs: List[torch.Tensor] = []
+
+        for link_id in link_ids:
+            pose = poses[link_id]
+            jac = jposes.new_zeros(angles.shape[0], 6, robot.dof)
+            sel = robot.links[link_id].ancestor_non_fixed_joint_ids
+            jac[:, :, sel] = se3.adjoint(se3.inverse(pose)) @ jposes[:, :, sel]
+            jacs.append(jac)
+
+        return jacs, rets
+
+    class ForwardKinematics(torch.autograd.Function):
+        @classmethod
+        def forward(cls, ctx, angles):
+            poses = _forward_kinematics_helper(angles)
+            ctx.poses = poses
+            rets = tuple(poses[id] for id in link_ids)
+            ctx.rets = rets
+            return rets
+
+        @classmethod
+        def backward(cls, ctx, *grad_outputs):
+            if not hasattr(ctx, "jposes"):
+                ctx.jposes: torch.Tensor = _jforward_kinematics_helper(ctx.poses)
+            rets: tuple(torch.Tensor) = ctx.rets
+            grad_pose = grad_outputs[0].new_zeros(
+                grad_outputs[0].shape[0], 6, robot.dof
+            )
+            grad_input = grad_outputs[0].new_zeros(grad_outputs[0].shape[0], robot.dof)
+
+            for link_id, ret, grad_output in zip(link_ids, rets, grad_outputs):
+                ancestor_non_fixed_joint_ids = robot.links[
+                    link_id
+                ].ancestor_non_fixed_joint_ids
+                temp = se3.project(
+                    torch.cat(
+                        (grad_output @ ret.transpose(1, 2), grad_output[:, :, 3:]),
+                        dim=-1,
+                    )
+                ).unsqueeze(-1)
+                grad_pose[:, :, ancestor_non_fixed_joint_ids] += temp
+            grad_input[:, joint_ids] = (
+                ctx.jposes[:, :, joint_ids] * grad_pose[:, :, joint_ids]
+            ).sum(-2)
+
+            return grad_input
+
+    return (
+        ForwardKinematics,
+        _forward_kinematics_impl,
+        _jforward_kinematics_impl,
+        _forward_kinematics_helper,
+        _jforward_kinematics_helper,
+    )
+
+
+def get_forward_kinematics(robot: Robot, link_names: Optional[List[str]] = None):
+    ForwardKinematics, _, jforward_kinematics, _, _ = ForwardKinematicsFactory(
+        robot, link_names
+    )
+    forward_kinematics = ForwardKinematics.apply
+    return forward_kinematics, jforward_kinematics
--- theseus/embodied/robot/joint.py
+++ theseus/embodied/robot/joint.py
@@ -5,26 +5,96 @@
 
 import abc
 import warnings
-from typing import Optional
+from typing import List, Optional
 import torch
 
-from theseus.labs.lie_functional import so3, se3
+from theseus.labs.lie_functional import so3
+from theseus.constants import DeviceType
+
+
+class Link(abc.ABC):
+    def __init__(
+        self,
+        name: str,
+        id: Optional[int] = None,
+        parent_joint: "Joint" = None,
+        child_joints: Optional[List["Joint"]] = None,
+        dtype: Optional[torch.dtype] = None,
+    ):
+        self._name = name
+        self._id = id
+        self._parent_joint = parent_joint
+        self._child_joints = child_joints if child_joints else []
+        self._ancestor_links: List[Link] = []
+        self._ancestor_non_fixed_joint_ids: List[int] = []
+        self._dtype = dtype
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def parent_link(self) -> "Link":
+        return (
+            self._parent_joint.parent_link if self._parent_joint is not None else None
+        )
+
+    @property
+    def parent_joint(self) -> "Joint":
+        return self._parent_joint
+
+    @property
+    def child_joints(self) -> List["Joint"]:
+        return self._child_joints
+
+    @property
+    def ancestor_links(self) -> List["Link"]:
+        return self._ancestor_links
+
+    @property
+    def ancestor_non_fixed_joint_ids(self):
+        return self._ancestor_non_fixed_joint_ids
+
+    @property
+    def dtype(self) -> torch.dtype:
+        return self._dtype
+
+    def _update_ancestor_links(self):
+        curr = self.parent_link
+        self._ancestor_links = []
+        while curr is not None:
+            self._ancestor_links.insert(0, curr)
+            curr = curr.parent_link
+
+    def _add_child_joint(self, child_joint: "Joint"):
+        self._child_joints.append(child_joint)
+
+    def _remove_child_joint(self, child_joint: "Joint"):
+        self._child_joints.remove(child_joint)
 
 
 class Joint(abc.ABC):
     def __init__(
         self,
         name: str,
-        parent: int = -1,
-        child: int = -1,
+        dof: int,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
         origin: Optional[torch.Tensor] = None,
         dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
     ):
         if origin is None and dtype is None:
             dtype = torch.get_default_dtype()
+        if origin is None and device is None:
+            device = torch.device("cpu")
         if origin is not None:
-            if origin.shape[0] != 1 or not se3.check_group_tensor(origin):
-                raise ValueError("Origin must be an element of SE(3).")
+            self._origin = origin
 
             if dtype is not None and origin.dtype != dtype:
                 warnings.warn(
@@ -32,30 +102,49 @@ class Joint(abc.ABC):
                     "tensor.dtype will take precendence."
                 )
             dtype = origin.dtype
+
+            if device is not None and origin.device != device:
+                warnings.warn(
+                    f"tensor.device {origin.device} does not match given device {device}, "
+                    "tensor.device will take precendence."
+                )
+            dtype = origin.dtype
+            device = origin.device
         else:
-            origin = torch.zeros(1, 3, 4, dtype=dtype)
+            origin = torch.zeros(1, 3, 4, dtype=dtype, device=device)
             origin[:, 0, 0] = 1
             origin[:, 1, 1] = 1
             origin[:, 2, 2] = 1
 
         self._name = name
-        self._parent = parent
-        self._child = child
+        self._dof = dof
+        self._id = id
+        self._parent_link = parent_link
+        self._child_link = child_link
         self._origin = origin
         self._dtype = dtype
-        self._axis: torch.Tensor = torch.zeros(6, 1, dtype=dtype)
+        self._device = torch.device(device)
+        self._axis: torch.Tensor = torch.zeros(6, self.dof, dtype=dtype, device=device)
 
     @property
     def name(self) -> str:
         return self._name
 
     @property
-    def parent(self) -> int:
-        return self._parent
+    def dof(self) -> int:
+        return self._dof
 
     @property
-    def child(self) -> int:
-        return self._child
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def parent_link(self) -> Optional[Link]:
+        return self._parent_link
+
+    @property
+    def child_link(self) -> Optional[Link]:
+        return self._child_link
 
     @property
     def origin(self) -> torch.Tensor:
@@ -66,43 +155,58 @@ class Joint(abc.ABC):
         return self._dtype
 
     @property
+    def device(self) -> torch.device:
+        return self._device
+
+    @property
     def axis(self) -> torch.Tensor:
         return self._axis
 
-    def set_parent(self, parent: int):
-        self._parent = parent
-
-    def set_child(self, child: int):
-        self._child = child
-
     @abc.abstractmethod
-    def dof(self) -> int:
+    def relative_pose(self, angle: Optional[torch.Tensor] = None) -> torch.Tensor:
         pass
 
-    @abc.abstractmethod
-    def relative_pose(self, angle: torch.Tensor) -> torch.Tensor:
-        pass
+
+class FixedJoint(Joint):
+    def __init__(
+        self,
+        name: str,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
+        origin: Optional[torch.Tensor] = None,
+        dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
+    ):
+        super().__init__(name, 0, id, parent_link, child_link, origin, dtype, device)
+
+    def relative_pose(self, *args) -> torch.Tensor:
+        if len(args) != 0:
+            raise ValueError("Fixed joint has no inputs.")
+        return self.origin
 
 
 class _RevoluteJointImpl(Joint):
     def __init__(
         self,
         name: str,
-        parent: int = -1,
-        child: int = -1,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
         origin: Optional[torch.Tensor] = None,
         dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
     ):
-        super().__init__(name, parent, child, origin, dtype)
-
-    def dof(self) -> int:
-        return 1
+        super().__init__(name, 1, id, parent_link, child_link, origin, dtype, device)
 
     @abc.abstractmethod
     def _rotation_impl(self, angle: torch.Tensor) -> torch.Tensor:
         pass
 
-    def relative_pose(self, angle: torch.Tensor) -> torch.Tensor:
+    def relative_pose(self, *args) -> torch.Tensor:
+        if len(args) != 1:
+            raise ValueError("Revolute joint requires one input.")
+        angle: torch.Tensor = args[0]
         rot = self._rotation_impl(angle)
         ret = angle.new_empty(angle.shape[0], 3, 4)
         ret[:, :, :3] = self.origin[:, :, :3] @ rot
@@ -115,10 +219,12 @@ class RevoluteJoint(_RevoluteJointImpl):
         self,
         name: str,
         revolute_axis: torch.Tensor,
-        parent: int = -1,
-        child: int = -1,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
         origin: Optional[torch.Tensor] = None,
         dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
     ):
         if revolute_axis.ndim == 1:
             revolute_axis = revolute_axis.view(-1, 1)
@@ -126,10 +232,13 @@ class RevoluteJoint(_RevoluteJointImpl):
         if revolute_axis.ndim != 2 or revolute_axis.shape != (3, 1):
             raise ValueError("The revolute axis must be a 3-D vector.")
 
-        super().__init__(name, parent, child, origin, dtype)
+        super().__init__(name, id, parent_link, child_link, origin, dtype, device)
 
         if revolute_axis.dtype != self.dtype:
             raise ValueError(f"The dtype of revolute_axis should be {self.dtype}.")
+
+        if revolute_axis.device != self.device:
+            raise ValueError(f"The device of revolute_axis should be {self.device}.")
 
         self._axis[3:] = revolute_axis
 
@@ -145,12 +254,14 @@ class RevoluteJointX(_RevoluteJointImpl):
     def __init__(
         self,
         name: str,
-        parent: int = -1,
-        child: int = -1,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
         origin: Optional[torch.Tensor] = None,
         dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
     ):
-        super().__init__(name, parent, child, origin, dtype)
+        super().__init__(name, id, parent_link, child_link, origin, dtype, device)
         self._axis[3] = 1
 
     def _rotation_impl(self, angle: torch.Tensor) -> torch.Tensor:
@@ -171,12 +282,14 @@ class RevoluteJointY(_RevoluteJointImpl):
     def __init__(
         self,
         name: str,
-        parent: int = -1,
-        child: int = -1,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
         origin: Optional[torch.Tensor] = None,
         dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
     ):
-        super().__init__(name, parent, child, origin, dtype)
+        super().__init__(name, id, parent_link, child_link, origin, dtype, device)
         self._axis[4] = 1
 
     def _rotation_impl(self, angle: torch.Tensor) -> torch.Tensor:
@@ -197,12 +310,14 @@ class RevoluteJointZ(_RevoluteJointImpl):
     def __init__(
         self,
         name: str,
-        parent: int = -1,
-        child: int = -1,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
         origin: Optional[torch.Tensor] = None,
         dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
     ):
-        super().__init__(name, parent, child, origin, dtype)
+        super().__init__(name, id, parent_link, child_link, origin, dtype, device)
         self._axis[5] = 1
 
     def _rotation_impl(self, angle: torch.Tensor) -> torch.Tensor:
@@ -223,21 +338,23 @@ class _PrismaticJointImpl(Joint):
     def __init__(
         self,
         name: str,
-        parent: int = -1,
-        child: int = -1,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
         origin: Optional[torch.Tensor] = None,
         dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
     ):
-        super().__init__(name, parent, child, origin, dtype)
-
-    def dof(self) -> int:
-        return 1
+        super().__init__(name, 1, id, parent_link, child_link, origin, dtype, device)
 
     @abc.abstractmethod
     def _translation_impl(self, angle: torch.Tensor) -> torch.Tensor:
         pass
 
-    def relative_pose(self, angle: torch.Tensor) -> torch.Tensor:
+    def relative_pose(self, *args) -> torch.Tensor:
+        if len(args) != 1:
+            raise ValueError("Prismatic joint requires one input.")
+        angle: torch.Tensor = args[0]
         trans = self._translation_impl(angle)
         ret = angle.new_empty(angle.shape[0], 3, 4)
         ret[:, :, :3] = self.origin[:, :, :3]
@@ -252,10 +369,12 @@ class PrismaticJoint(_PrismaticJointImpl):
         self,
         name: str,
         prismatic_axis: torch.Tensor,
-        parent: int = -1,
-        child: int = -1,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
         origin: Optional[torch.Tensor] = None,
         dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
     ):
         if prismatic_axis.ndim == 1:
             prismatic_axis = prismatic_axis.view(-1, 1)
@@ -263,10 +382,13 @@ class PrismaticJoint(_PrismaticJointImpl):
         if prismatic_axis.ndim != 2 or prismatic_axis.shape != (3, 1):
             raise ValueError("The prismatic axis must be a 3-D vector.")
 
-        super().__init__(name, parent, child, origin, dtype)
+        super().__init__(name, id, parent_link, child_link, origin, dtype, device)
 
         if prismatic_axis.dtype != self.dtype:
             raise ValueError(f"The dtype of prismatic_axis should be {self.dtype}.")
+
+        if prismatic_axis.device != self.device:
+            raise ValueError(f"The device of prismatic_axis should be {self.device}.")
 
         self._axis[:3] = prismatic_axis
 
@@ -282,12 +404,14 @@ class PrismaticJointX(_PrismaticJointImpl):
     def __init__(
         self,
         name: str,
-        parent: int = -1,
-        child: int = -1,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
         origin: Optional[torch.Tensor] = None,
         dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
     ):
-        super().__init__(name, parent, child, origin, dtype)
+        super().__init__(name, id, parent_link, child_link, origin, dtype, device)
         self._axis[3] = 1
 
     def _translation_impl(self, angle: torch.Tensor) -> torch.Tensor:
@@ -299,7 +423,10 @@ class PrismaticJointX(_PrismaticJointImpl):
         trans[:, 0] = angle
         return trans
 
-    def relative_pose(self, angle: torch.Tensor) -> torch.Tensor:
+    def relative_pose(self, *args) -> torch.Tensor:
+        if len(args) != 1:
+            raise ValueError("Prismatic joint requires one input.")
+        angle: torch.Tensor = args[0]
         if angle.ndim == 1:
             angle = angle.view(-1, 1)
         if angle.ndim != 2 or angle.shape[1] != 1:
@@ -314,12 +441,14 @@ class PrismaticJointY(_PrismaticJointImpl):
     def __init__(
         self,
         name: str,
-        parent: int = -1,
-        child: int = -1,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
         origin: Optional[torch.Tensor] = None,
         dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
     ):
-        super().__init__(name, parent, child, origin, dtype)
+        super().__init__(name, id, parent_link, child_link, origin, dtype, device)
         self._axis[4] = 1
 
     def _translation_impl(self, angle: torch.Tensor) -> torch.Tensor:
@@ -331,7 +460,10 @@ class PrismaticJointY(_PrismaticJointImpl):
         trans[:, 1] = angle
         return trans
 
-    def relative_pose(self, angle: torch.Tensor) -> torch.Tensor:
+    def relative_pose(self, *args) -> torch.Tensor:
+        if len(args) != 1:
+            raise ValueError("Prismatic joint requires one input.")
+        angle: torch.Tensor = args[0]
         if angle.ndim == 1:
             angle = angle.view(-1, 1)
         if angle.ndim != 2 or angle.shape[1] != 1:
@@ -346,12 +478,14 @@ class PrismaticJointZ(_PrismaticJointImpl):
     def __init__(
         self,
         name: str,
-        parent: int = -1,
-        child: int = -1,
+        id: Optional[int] = None,
+        parent_link: Optional[Link] = None,
+        child_link: Optional[Link] = None,
         origin: Optional[torch.Tensor] = None,
         dtype: Optional[torch.dtype] = None,
+        device: DeviceType = None,
     ):
-        super().__init__(name, parent, child, origin, dtype)
+        super().__init__(name, id, parent_link, child_link, origin, dtype, device)
         self._axis[5] = 1
 
     def _translation_impl(self, angle: torch.Tensor) -> torch.Tensor:
@@ -363,7 +497,10 @@ class PrismaticJointZ(_PrismaticJointImpl):
         trans[:, 2] = angle
         return trans
 
-    def relative_pose(self, angle: torch.Tensor) -> torch.Tensor:
+    def relative_pose(self, *args) -> torch.Tensor:
+        if len(args) != 1:
+            raise ValueError("Prismatic joint requires one input.")
+        angle: torch.Tensor = args[0]
         if angle.ndim == 1:
             angle = angle.view(-1, 1)
         if angle.ndim != 2 or angle.shape[1] != 1:

5910e1e69aac3ab5226fcf62fbab412f6d5c998d
Luis Pineda <4759586+luisenp@users.noreply.github.com>
2023-03-08 12:28:39 -0800

    Added TheseusLayer.verify_jacobians() method.

--- theseus/utils/utils.py
+++ theseus/utils/utils.py
@@ -131,11 +131,13 @@ def numeric_grad(
 
 
 # Updates the given variable with a random tensor of the same shape as the original.
-def _rand_fill_(v: th.Variable):
+def _rand_fill_(v: th.Variable, batch_size: int):
     if isinstance(v, (th.SE2, th.SO3, th.SE3, th.SO3)):
-        v.update(v.rand(v.shape[0], dtype=v.dtype, device=v.device).tensor)
+        v.update(v.rand(batch_size, dtype=v.dtype, device=v.device).tensor)
     else:
-        v.update(torch.rand_like(v.tensor))
+        v.update(
+            torch.rand((batch_size,) + v.shape[1:], dtype=v.dtype, device=v.device)
+        )
 
 
 # Automatically checks the jacobians of the given cost function a number of times.
@@ -161,18 +163,22 @@ def check_jacobians(cf: th.CostFunction, num_checks: int = 1, tol: float = 1.0e-
     with _tmp_tensors(optim_vars), _tmp_tensors(aux_vars):
         for _ in range(num_checks):
             for v in optim_vars + aux_vars:
-                _rand_fill_(v)
+                _rand_fill_(v, 1)
 
             autograd_jac = torch.autograd.functional.jacobian(
                 autograd_fn, tuple(v.tensor for v in optim_vars)
             )
             jac, _ = cf.jacobians()
             for idx, v in enumerate(optim_vars):
-                j1 = jac[idx]
+                j1 = jac[idx][0]
                 j2 = autograd_jac[idx]
-                aux = torch.arange(j1.shape[0])
-                sparse_j2 = v.project(j2[aux, :, aux, :], is_sparse=True)
-                if (j1 - sparse_j2).abs().max() > tol:
+                # In some "unfriendly" cost functions, the error's batch size could
+                # be different than the optim/aux vars batch size, if they save
+                # tensors that are not exposed as Theseus variables. To avoid issues,
+                # we just check the first element of the batch.
+                j2_sparse = j2[0, :, 0, :]
+                j2_sparse_manifold = v.project(j2_sparse, is_sparse=True)
+                if (j1 - j2_sparse_manifold).abs().max() > tol:
                     raise RuntimeError(
                         f"Jacobian for variable {v.name} appears incorrect to the "
                         "given tolerance."

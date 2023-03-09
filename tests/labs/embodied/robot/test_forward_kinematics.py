992cd124de063d6bbf80029dad2ca30d01efb3f4
Taosha Fan <fantaosha@gmail.com>
2023-03-09 11:26:44 -0800

    move files to labs

--- tests/labs/embodied/robot/test_forward_kinematics.py
+++ tests/labs/embodied/robot/test_forward_kinematics.py
@@ -7,21 +7,23 @@ import pytest
 import os
 import torch
 from tests.decorators import run_if_labs
-from theseus.labs.lie.functional.constants import TEST_EPS
-from theseus.embodied.robot.forward_kinematics import Robot
-from theseus.embodied.robot.forward_kinematics import (
-    get_forward_kinematics,
-    ForwardKinematicsFactory,
-)
 
 
-URDF_REL_PATH = "../kinematics/data/panda_no_gripper.urdf"
+URDF_REL_PATH = "../../../embodied/kinematics/data/panda_no_gripper.urdf"
 urdf_path = os.path.join(os.path.dirname(__file__), URDF_REL_PATH)
 
 
+@run_if_labs()
 @pytest.mark.parametrize("batch_size", [1, 20, 40])
 @pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
 def test_backward(batch_size: int, dtype: torch.dtype):
+    from theseus.labs.lie.functional.constants import TEST_EPS
+    from theseus.labs.embodied.robot.forward_kinematics import Robot
+    from theseus.labs.embodied.robot.forward_kinematics import (
+        get_forward_kinematics,
+        ForwardKinematicsFactory,
+    )
+
     robot = Robot.from_urdf_file(urdf_path, dtype)
     selected_links = ["panda_link2", "panda_link5", "panda_virtual_ee_link"]
     _, fkin_impl, _, _, _ = ForwardKinematicsFactory(robot, selected_links)
@@ -56,6 +58,8 @@ def test_backward(batch_size: int, dtype: torch.dtype):
 @pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
 def test_jacobian(batch_size: int, dtype: torch.dtype):
     from theseus.labs.lie.functional import se3
+    from theseus.labs.embodied.robot.forward_kinematics import Robot
+    from theseus.labs.embodied.robot.forward_kinematics import get_forward_kinematics
 
     robot = Robot.from_urdf_file(urdf_path, dtype)
     selected_links = ["panda_link2", "panda_link5", "panda_virtual_ee_link"]

2d39bfa66cd4344a018317b2da69f4db1e30424b
Luis Pineda <4759586+luisenp@users.noreply.github.com>
2023-03-13 10:29:43 -0700

    Slight efficiency improvement to Nonholonomic cost.

--- theseus/embodied/motionmodel/misc.py
+++ theseus/embodied/motionmodel/misc.py
@@ -33,24 +33,18 @@ class Nonholonomic(CostFunction):
     def dim(self):
         return 1
 
-    def _compute_error(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def _compute_error(
+        self,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[torch.Tensor]]:
         vel = self.vel.tensor
         if isinstance(self.pose, SE2):
             error = vel[:, 1]
-            J_error_pose = error.new_zeros(error.shape[0], 1, 3)
-            J_error_vel = error.new_zeros(error.shape[0], 1, 3)
-            J_error_vel[:, 0, 1] = 1
+            cos, sin = None, None
         else:
             cos = self.pose[:, 2].cos()
             sin = self.pose[:, 2].sin()
             error = vel[:, 1] * cos - vel[:, 0] * sin
-            # Computing jacobians
-            J_error_pose = self.vel.tensor.new_zeros(vel.shape[0], 1, 3)
-            J_error_vel = self.vel.tensor.new_zeros(vel.shape[0], 1, 3)
-            J_error_pose[:, 0, 2] = -(vel[:, 1] * sin + vel[:, 0] * cos)
-            J_error_vel[:, 0, 0] = -sin
-            J_error_vel[:, 0, 1] = cos
-        return error.view(-1, 1), J_error_pose, J_error_vel
+        return error.view(-1, 1), cos, sin
 
     def error(self) -> torch.Tensor:
         return self._compute_error()[0]
@@ -59,8 +53,17 @@ class Nonholonomic(CostFunction):
         # Pre-allocate jacobian tensors
         batch_size = self.vel.shape[0]
         Jvel = self.vel.tensor.new_zeros(batch_size, 1, 3)
-
-        error, Jpose, Jvel = self._compute_error()
+        error, cos, sin = self._compute_error()
+        if isinstance(self.pose, SE2):
+            Jpose = error.new_zeros(error.shape[0], 1, 3)
+            Jvel = error.new_zeros(error.shape[0], 1, 3)
+            Jvel[:, 0, 1] = 1
+        else:
+            Jpose = self.vel.tensor.new_zeros(self.vel.shape[0], 1, 3)
+            Jvel = self.vel.tensor.new_zeros(self.vel.shape[0], 1, 3)
+            Jpose[:, 0, 2] = -(self.vel[:, 1] * sin + self.vel[:, 0] * cos)
+            Jvel[:, 0, 0] = -sin
+            Jvel[:, 0, 1] = cos
         return [Jpose, Jvel], error
 
     def _copy_impl(self, new_name: Optional[str] = None) -> "Nonholonomic":

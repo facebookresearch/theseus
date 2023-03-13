6f561944b7b1d7f12469ee42d0894d84fd753c20
Luis Pineda <4759586+luisenp@users.noreply.github.com>
2023-03-13 10:01:12 -0700

    Added different Nonholonomic versions depending on whether poses are SE2 or Vector.

--- tests/embodied/motionmodel/test_misc.py
+++ tests/embodied/motionmodel/test_misc.py
@@ -7,8 +7,13 @@ from theseus.utils import check_jacobians
 def test_nonholonomic():
     rng = torch.Generator()
     rng.manual_seed(0)
+    # Check SE2 pose version
     pose = th.SE2.rand(10, generator=rng)
     vel = th.Vector.rand(10, 3, generator=rng)
     w = th.ScaleCostWeight(1.0)
+    cf = th.eb.Nonholonomic(pose, vel, w)
+    check_jacobians(cf, num_checks=100, tol=1e-5)
+    # Check Vector3 pose version
+    pose = th.Vector.rand(10, 3, generator=rng)
     cf = th.eb.Nonholonomic(pose, vel, w)
     check_jacobians(cf, num_checks=100, tol=1e-5)
--- theseus/embodied/motionmodel/misc.py
+++ theseus/embodied/motionmodel/misc.py
@@ -3,27 +3,27 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Union
 
 import torch
 
 from theseus.core import CostFunction, CostWeight
-from theseus.geometry import SE2, Vector
+from theseus.geometry import SE2, Point3, Vector
 
 
 class Nonholonomic(CostFunction):
     def __init__(
         self,
-        pose: SE2,
-        vel: Vector,
+        pose: Union[SE2, Point3, Vector],
+        vel: Union[Point3, Vector],
         cost_weight: CostWeight,
         name: Optional[str] = None,
     ):
         super().__init__(cost_weight, name=name)
-        if vel.dof() != 3:
+        if vel.dof() != 3 or pose.dof() != 3:
             raise ValueError(
-                "Nonholonomic only accepts 3D velocity vectors, "
-                "representing vx, vy, vtheta."
+                "Nonholonomic only accepts 3D velocity or poses (x, y, z dims). "
+                "Poses can either be SE2 or Vector variables. Velocities only Vector."
             )
         self.pose = pose
         self.vel = vel
@@ -35,15 +35,21 @@ class Nonholonomic(CostFunction):
 
     def _compute_error(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         vel = self.vel.tensor
-        cos, sin = self.pose.rotation.to_cos_sin()
-        J_theta_pose: List[torch.Tensor] = []
-        self.pose.theta(jacobians=J_theta_pose)
-        error = vel[:, 1] * cos - vel[:, 0] * sin
-        J_error_theta = -(vel[:, 1] * sin + vel[:, 0] * cos).view(-1, 1, 1)
-        J_error_pose = J_error_theta.matmul(J_theta_pose[0])
-        J_error_vel = self.vel.tensor.new_zeros(vel.shape[0], 1, 3)
-        J_error_vel[:, 0, 0] = -sin
-        J_error_vel[:, 0, 1] = cos
+        if isinstance(self.pose, SE2):
+            error = vel[:, 1]
+            J_error_pose = error.new_zeros(error.shape[0], 1, 3)
+            J_error_vel = error.new_zeros(error.shape[0], 1, 3)
+            J_error_vel[:, 0, 1] = 1
+        else:
+            cos = self.pose[:, 2].cos()
+            sin = self.pose[:, 2].sin()
+            error = vel[:, 1] * cos - vel[:, 0] * sin
+            # Computing jacobians
+            J_error_pose = self.vel.tensor.new_zeros(vel.shape[0], 1, 3)
+            J_error_vel = self.vel.tensor.new_zeros(vel.shape[0], 1, 3)
+            J_error_pose[:, 0, 2] = -(vel[:, 1] * sin + vel[:, 0] * cos)
+            J_error_vel[:, 0, 0] = -sin
+            J_error_vel[:, 0, 1] = cos
         return error.view(-1, 1), J_error_pose, J_error_vel
 
     def error(self) -> torch.Tensor:

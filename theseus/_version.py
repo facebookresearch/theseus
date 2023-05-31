c6518a193ae61a3508feee58d8fb4ff70f90ea86
Luis Pineda <4759586+luisenp@users.noreply.github.com>
2023-05-31 11:35:24 -0700

    Simplified version parsing for build script.

--- theseus/_version.py
+++ theseus/_version.py
@@ -3,15 +3,11 @@ import warnings
 from torch import __version__ as _torch_version
 from semantic_version import Version
 
+if Version(_torch_version) < Version("2.0.0"):
+    warnings.warn(
+        "Using torch < 2.0 is deprecated and support will be discontinued "
+        " in future releases.",
+        FutureWarning,
+    )
 
-def get_version() -> str:
-    if Version(_torch_version) < Version("2.0.0"):
-        warnings.warn(
-            "Using torch < 2.0 is deprecated and support will be discontinued "
-            " in future releases.",
-            FutureWarning,
-        )
-    return "0.2.0.dev0"  # __THESEUS_VERSION__
-
-
-__version__ = get_version()
+__version__ = "0.2.0.dev0"

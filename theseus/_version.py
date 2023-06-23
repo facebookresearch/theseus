103219c2df6db8c924fcc16649faa0e5380b5402
Luis Pineda <4759586+luisenp@users.noreply.github.com>
2023-06-22 21:38:20 -0700

    Updated setup.py and bump theseus version to 0.2.0.rc1.

--- theseus/_version.py
+++ theseus/_version.py
@@ -1,3 +1,7 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 import warnings
 
 from torch import __version__ as _torch_version
@@ -5,9 +9,9 @@ from semantic_version import Version
 
 if Version(_torch_version) < Version("2.0.0"):
     warnings.warn(
-        "Using torch < 2.0 is deprecated and support will be discontinued "
-        " in future releases.",
+        "Using torch < 2.0 for theseus is deprecated and compatibility will be "
+        "discontinued in future releases.",
         FutureWarning,
     )
 
-__version__ = "0.2.0.dev0"
+__version__ = "0.2.0.rc1"

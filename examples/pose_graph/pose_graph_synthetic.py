fixed the rotation and translation order in pgo datasets (#560)
* fixed the rotation and translation order in pgo datasets

---------

Co-authored-by: Sudharshan Suresh <suddhus@gmail.com>

examples/pose_graph/pose_graph_synthetic.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# SE(3) convention in this example is translation then rotation
+
 import cProfile
 import io
 import logging
@@ -121,8 +123,8 @@ def run(cfg: omegaconf.OmegaConf):
     dtype = torch.float64
     pg, _ = theg.PoseGraphDataset.generate_synthetic_3D(
         num_poses=cfg.num_poses,
-        rotation_noise=cfg.rotation_noise,
         translation_noise=cfg.translation_noise,
+        rotation_noise=cfg.rotation_noise,
         loop_closure_ratio=cfg.loop_closure_ratio,
         loop_closure_outlier_ratio=cfg.loop_closure_outlier_ratio,
         batch_size=cfg.batch_size,

theseus/utils/examples/pose_graph/dataset.py
@@ -237,8 +237,8 @@ class PoseGraphDataset:
     @staticmethod
     def generate_synthetic_3D(
         num_poses: int,
-        rotation_noise: float = 0.05,
-        translation_noise: float = 0.1,
+        translation_noise: float = 0.05,
+        rotation_noise: float = 0.1,
         loop_closure_ratio: float = 0.2,
         loop_closure_outlier_ratio: float = 0.05,
         max_num_loop_closures: int = 10,
@@ -282,9 +282,9 @@ class PoseGraphDataset:
             noise_relative_pose = th.SE3.exp_map(
                 torch.cat(
                     [
-                        rotation_noise
-                        * (2 * torch.rand(dataset_size, 3, dtype=dtype) - 1),
                         translation_noise
+                        * (2.0 * torch.rand(dataset_size, 3, dtype=dtype) - 1),
+                        rotation_noise
                         * (2.0 * torch.rand(dataset_size, 3, dtype=dtype) - 1),
                     ],
                     dim=1,
@@ -322,9 +322,9 @@ class PoseGraphDataset:
                         noise_relative_pose = th.SE3.exp_map(
                             torch.cat(
                                 [
-                                    rotation_noise
-                                    * (2 * torch.rand(1, 3, dtype=dtype) - 1),
                                     translation_noise
+                                    * (2.0 * torch.rand(1, 3, dtype=dtype) - 1),
+                                    rotation_noise
                                     * (2.0 * torch.rand(1, 3, dtype=dtype) - 1),
                                 ],
                                 dim=1,
@@ -354,8 +354,8 @@ class PoseGraphDataset:
             noise_pose = th.SE3.exp_map(
                 torch.cat(
                     [
-                        rotation_noise * (2 * torch.rand(1, 3, dtype=dtype) - 1),
                         translation_noise * (2.0 * torch.rand(1, 3, dtype=dtype) - 1),
+                        rotation_noise * (2.0 * torch.rand(1, 3, dtype=dtype) - 1),
                     ],
                     dim=1,
                 )
556d61bce8115d5216fcb900040ce52a1493b752
Luis Pineda <4759586+luisenp@users.noreply.github.com>
2023-07-07 10:44:31 -0700

    Removed unnecessary code from __init__.

--- theseus/third_party/semantic_version/__init__.py
+++ theseus/third_party/semantic_version/__init__.py
@@ -4,14 +4,3 @@
 # type: ignore
 
 from .base import NpmSpec, SimpleSpec, Spec, SpecItem, Version, compare, match, validate
-
-__author__ = "Raphaël Barrois <raphael.barrois+semver@polytechnique.org>"
-try:
-    # Python 3.8+
-    from importlib.metadata import version
-
-    __version__ = version("semantic_version")
-except ImportError:
-    import pkg_resources
-
-    __version__ = pkg_resources.get_distribution("semantic_version").version
